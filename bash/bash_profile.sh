ba016ce9a2489f61ab9e5f1dad352dc4d66cc73d
Derek Wan <d.wan@icloud.com>
2021-01-31 22:30:17 +0800

    Remove PAT (#94)
    * Remove token
    
    * Bump version: 0.4.48 → 0.4.49

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -4,7 +4,6 @@
 export EDITOR=vim
 export FLASK_DEBUG=1
 export GIST_ID=690a59ef26208e43fa880c874e01c18c
-export GITHUB_TOKEN=dedfdf4d433b74ff3d18b8cb2089a52c0c5dfc5a
 export MKL_NUM_THREADS=1
 export PATH_DROPBOX="/data/derek/Dropbox"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
