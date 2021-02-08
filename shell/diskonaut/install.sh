4fb917619508ea1fc18860261c58aa8e29fac2d3
Derek Wan <d.wan@icloud.com>
2021-02-08 23:26:44 +0800

    Add Diskonaut (#126)
    * Add diskonaut
    
    * Bump version: 0.5.24 → 0.5.25

---
+++ shell/diskonaut/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if ! command -v diskonaut >/dev/null 2>&1; then
+	file="$(find "$(git rev-parse --show-toplevel)" -path \*/rust/cargo-install.sh -type f)"
+	if [ -f "$file" ]; then
+		# shellcheck source=/dev/null
+		source "$file" diskonaut
+	else
+		echo "cargo-install.sh not found"
+	fi
+fi
