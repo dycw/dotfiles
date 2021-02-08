5a717ff35c30865edd9d71828f4ae1a8b34bd625
Derek Wan <d.wan@icloud.com>
2021-02-08 22:07:01 +0800

    Add Docker (#125)
    * Add docker/install.sh
    
    * Add --yes
    
    * Bump version: 0.5.23 → 0.5.24

---
+++ docker/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if ! command -v docker >/dev/null 2>&1; then
+	sudo apt-get update
+	sudo apt-get --yes install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo apt-key fingerprint 0EBFCD88
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get --yes install docker-ce docker-ce-cli containerd.io
+fi
