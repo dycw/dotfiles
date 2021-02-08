c0f27dfbc27b9bb4d2b3129aecfc29a8c0a7bbfa
Derek Wan <d.wan@icloud.com>
2021-02-09 00:11:16 +0800

    Homebrew superceded Linuxbrew (#129)
    * Remove linuxbrew, it's all homebrew now
    
    * Add env.sh
    
    * Bump version: 0.5.27 → 0.5.28

--- brew/env.sh
+++ brew/env.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-path="$HOME/.linuxbrew"
-if [ -d "$path" ]; then
-	export LINUXBREWHOME="$path"
-	export LD_LIBRARY_PATH=$LINUXBREWHOME/lib64:$LINUXBREWHOME/lib:$LD_LIBRARY_PATH
-	export MANPATH=$LINUXBREWHOME/man:$MANPATH
-	export PATH=$LINUXBREWHOME/bin:$PATH
-	export PKG_CONFIG_PATH=$LINUXBREWHOME/lib64/pkgconfig:$LINUXBREWHOME/lib/pkgconfig:$PKG_CONFIG_PATH
-	# Until LinuxBrew is fixed, the following is required.
-	# See: https://github.com/Homebrew/linuxbrew/issues/47
-	export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig:$PKG_CONFIG_PATH
+file=/home/linuxbrew/.linuxbrew/bin/brew
+if [ -f "$file" ]; then
+	eval "$("$file" shellenv)"
 fi
--- brew/install.sh
+++ brew/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 if ! command -v brew >/dev/null 2>&1; then
-	sudo apt-get update
-	sudo apt-get --yes install autoconf autoconf-archive automake bison build-essential cmake curl flex gettext git make ruby scons libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev libtool
-	git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
