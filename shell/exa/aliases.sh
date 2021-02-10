8e5f217372cc7346d61a910c88152ebaa16a2ff5
Derek Wan <d.wan@icloud.com>
2021-02-10 22:05:03 +0800

    Fix exa aliases; some need single quotes (#135)
    * Some exa alises need single quotes
    
    * Fix another exa aliase
    
    * Bump version: 0.5.33 → 0.5.34

--- shell/exa/aliases.sh
+++ shell/exa/aliases.sh
@@ -3,10 +3,10 @@
 if command -v exa >/dev/null 2>&1; then
 	_EXA_ARGS="--classify --group-directories-first"
 	_EXA_SHORT_ARGS="$_EXA_ARGS --across"
-	alias l="exa \$_EXA_SHORT_ARGS --git-ignore"
-	alias la="exa \$_EXA_SHORT_ARGS --all"
-	_EXA_LONG_ARGS="\$_EXA_ARGS --git --group --header --long --time-style=long-iso"
-	alias ll="exa \$_EXA_LONG_ARGS --git-ignore"
-	alias lla="exa \$_EXA_LONG_ARGS --all"
+	alias l='exa $_EXA_SHORT_ARGS --git-ignore'
+	alias la='exa $_EXA_SHORT_ARGS --all'
+	_EXA_LONG_ARGS="$_EXA_ARGS --git --group --header --long --time-style=long-iso"
+	alias ll='exa $_EXA_LONG_ARGS --git-ignore'
+	alias lla='exa $_EXA_LONG_ARGS --all'
 	alias lal="lla"
 fi
