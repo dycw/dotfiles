f33c0848185700d1275ff91bec947a4ba91a62fb
Derek Wan <d.wan@icloud.com>
2021-02-10 22:59:48 +0800

    Add cdr for repo root (#137)
    * Add cdr for repo root
    
    * Bump version: 0.5.35 → 0.5.36

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2139,SC2140
 
+alias cdr='cd $(git rev-parse --show-toplevel)'
+
 # https://gist.github.com/mwhite/6887990
 
 for al in $(git --list-cmds=alias); do
