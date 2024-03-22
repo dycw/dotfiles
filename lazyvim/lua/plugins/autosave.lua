7969ef24d66ae101dee9d6237e83fd4ba090e597
Derek Wan <d.wan@icloud.com>
2024-03-23 01:53:27 +0800

    Add `lazyvim` (#7)

---
+++ lazyvim/lua/plugins/autosave.lua
@@ -0,0 +1,9 @@
+return {
+  "okuuva/auto-save.nvim",
+  config = function()
+    require("auto-save").setup()
+  end,
+  keys = {
+    { "<Leader>a", "<Cmd>ASToggle<CR>", desc = "Toggle AutoSave" },
+  },
+}
