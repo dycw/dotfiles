4e0aaa43123c160c11de892acf47767c660965a4
Derek Wan <d.wan@icloud.com>
2024-03-23 03:43:20 +0800

    more

--- pytest/aliases.sh
+++ pytest/aliases.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 alias pyt='pytest -ra -vv --color=yes --strict-markers'
 alias pytc='pytest -ra -vv --color=yes --strict-markers --no-cov'
 alias pytcf='pytest -ra -vv --color=yes --strict-markers --no-cov -f'
