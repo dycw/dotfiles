442a731e794d164667872f5c34c11925ca4179cd
Derek Wan <d.wan@icloud.com>
2024-03-23 15:34:37 +0800

    more

--- ipython/startup.py
+++ ipython/startup.py
@@ -3,11 +3,71 @@ from __future__ import annotations  # noqa: INP001
 import abc
 import sys
 from abc import ABC, ABCMeta
+from collections import (
+    Counter,
+    defaultdict,
+    deque,
+)
+from collections.abc import (
+    AsyncGenerator,
+    AsyncIterable,
+    AsyncIterator,
+    Awaitable,
+    Callable,
+    Collection,
+    Container,
+    Coroutine,
+    Generator,
+    Hashable,
+    ItemsView,
+    Iterable,
+    Iterator,
+    KeysView,
+    Mapping,
+    MappingView,
+    MutableMapping,
+    MutableSequence,
+    MutableSet,
+    Reversible,
+    Sequence,
+    Sized,
+    ValuesView,
+)
+from collections.abc import (
+    Set as AbstractSet,
+)
 from contextlib import suppress
 from pathlib import Path
 from subprocess import PIPE, CalledProcessError, check_output
 
 _ = [
+    Counter,
+    AbstractSet,
+    defaultdict,
+    deque,
+    AsyncGenerator,
+    AsyncIterable,
+    AsyncIterator,
+    Awaitable,
+    Callable,
+    Collection,
+    Container,
+    Coroutine,
+    Generator,
+    Hashable,
+    ItemsView,
+    Iterable,
+    Iterator,
+    KeysView,
+    Mapping,
+    MappingView,
+    MutableMapping,
+    MutableSequence,
+    MutableSet,
+    Reversible,
+    Sequence,
+    Sized,
+    ValuesView,
     abc,
     ABC,
     sys,
@@ -26,6 +86,30 @@ except ModuleNotFoundError:
     pass
 else:
     _ = [beartype]
+
+
+try:
+    from bidict import bidict  # type: ignore[]
+except ModuleNotFoundError:
+    pass
+else:
+    _ = [bidict]
+
+
+try:
+    from bs4 import BeautifulSoup  # type: ignore[]
+except ModuleNotFoundError:
+    pass
+else:
+    _ = [BeautifulSoup]
+
+
+try:
+    from cachetools import ttl_cache  # type: ignore[]
+except ModuleNotFoundError:
+    pass
+else:
+    _ = [ttl_cache]
 
 
 def _add_src_to_sys_path() -> None:
