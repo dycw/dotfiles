df1a5ce02a4ea270ce32da195dd00f90b9ae6f2f
Derek Wan <d.wan@icloud.com>
2024-03-24 09:54:53 +0800

    Move from `lazyvim` to `lunarvim` (#29)
    * Add lunarvim
    
    * Save
    
    * lint
    
    * guarded-brew
    
    * more
    
    * remove lazy

--- lazyvim/lua/plugins/sort.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sqve/sort.nvim",
-  keys = {
-    { "<Leader>so", "<Cmd>Sort<CR>", desc = "Sort", mode = { "n" } },
-    { "<Leader>so", "<Esc><Cmd>Sort<CR>", desc = "Sort", mode = { "v" } },
-    { "<Leader>sn", "<Cmd>Sort n<CR>", desc = "Sort (number)", mode = { "n" } },
-    { "<Leader>sn", "<Esc><Cmd>Sort n<CR>", desc = "Sort (number)", mode = { "v" } },
-  },
-}
