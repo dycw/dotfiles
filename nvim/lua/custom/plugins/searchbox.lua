46f66511920100d9314caa5857fd0c358bc1b826
Derek Wan <d.wan@icloud.com>
2024-03-24 23:47:09 +0800

    more

---
+++ nvim/lua/custom/plugins/searchbox.lua
@@ -0,0 +1,21 @@
+return {
+    "vonheikemen/searchbox.nvim",
+    config = function()
+        vim.keymap.set("n", "/", "<cmd>SearchBoxIncSearch<CR>", { noremap = true, silent = true })
+        require("searchbox").setup({
+            popup = {
+                position = { row = "10%", col = "50%" },
+                size = { width = "60%" },
+                relative = "editor",
+                border = {
+                    style = "rounded",
+                    text = {
+                        top = " Search ",
+                        top_align = "center",
+                    },
+                },
+            },
+        })
+    end,
+    dependencies = { "muniftanjim/nui.nvim" },
+}
