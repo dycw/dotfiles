5c186203b11e7fca9af995cac3ef7f8325cb5e8e
Derek Wan <d.wan@icloud.com>
2024-03-24 09:54:24 +0800

    remove lazy

--- lazyvim/lua/plugins/autosave.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "okuuva/auto-save.nvim",
-  config = function()
-    require("auto-save").setup()
-  end,
-  keys = {
-    { "<Leader>a", "<Cmd>ASToggle<CR>", desc = "Toggle AutoSave" },
-  },
-}
