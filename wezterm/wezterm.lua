b61925f82586abd982186e94b40980aa70b3a393
Derek Wan <d.wan@icloud.com>
2024-03-24 16:31:08 +0800

    Add `tmux` and nightly `lvim` plugins (#32)
    * More commits
    
    * updates
    
    * update lock
    
    * update lock
    
    * update lock
    
    * fix
    
    * fix
    
    * fix

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,40 +9,6 @@ config.audible_bell = "Disabled"
 config.initial_cols = 200
 config.initial_rows = 100
 config.font_size = 14.0
-config.keys = {
-	-- navigate panes
-	{
-		key = "h",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "j",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "k",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "l",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	-- new panes
-	{
-		key = "|",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "_",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-}
 
 -- and finally, return the configuration to wezterm
 return config
