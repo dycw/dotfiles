f0924d479f1bb1d25f278eefd7af1dcea833ad66
Derek Wan <d.wan@icloud.com>
2024-03-24 22:48:19 +0800

    Use kickstart

---
+++ nvim/lua/config.lua
@@ -0,0 +1,8 @@
+-- Use spaces instead of tabs
+vim.o.expandtab = true
+
+-- Set the number of spaces per tab
+vim.o.tabstop = 4
+
+-- Set the number of spaces used for each indentation level
+vim.o.shiftwidth = 4
