f1a4cee5ba8aa925f2c9a1c6b6640f90fdc7cd28
Derek Wan <d.wan@icloud.com>
2024-04-07 21:59:33 +0800

    more

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,7 +4,7 @@ local set = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- command mode
-set({ "n", "v" }, "<CR>", ":", opts)
+set({ "n", "v" }, "<CR>", "<Cmd>", opts)
 
 -- global marks
 local prefixes = "m'"
@@ -35,21 +35,21 @@ set("n", "]", "<Cmd>cnext<CR>", opts)
 set("n", "[", "<Cmd>cprev<CR>", opts)
 
 -- quit
-set({ "n", "v" }, "<C-q>", ":confirm q<CR>", opts)
-set("i", "<C-q>", ":confirm q<CR>", opts)
+set({ "n", "v" }, "<C-q>", "<Cmd>confirm q<CR>", opts)
+set("i", "<C-q>", "<Esc><Cmd>confirm q<CR>a", opts)
 
 -- save
-set({ "n", "v" }, "<C-s>", "<Cmd>:w<CR>", opts)
-set("i", "<C-s>", "<Esc><Cmd>:w<CR>a", opts)
+set({ "n", "v" }, "<C-s>", "<Cmd>w<CR>", opts)
+set("i", "<C-s>", "<Esc><Cmd>w<CR>a", opts)
 
 -- search highlight
 set("n", "<Esc>", "<Cmd>nohlsearch<CR>", opts)
 
 -- split windows
-set("n", "<C-w>h", ":set nosplitright<CR> :vsplit<CR> :set splitright<CR>", opts)
-set("n", "<C-w>j", ":set splitbelow<CR> :split<CR>", opts)
-set("n", "<C-w>k", ":set nosplitbelow<CR> :split<CR> :set splitbelow<CR>", opts)
-set("n", "<C-w>l", ":set splitright<CR> :vsplit<CR>", opts)
+set("n", "<C-w>h", "<Cmd>set nosplitright<CR> <Cmd>vsplit<CR> <Cmd>set splitright<CR>", opts)
+set("n", "<C-w>j", "<Cmd>set splitbelow<CR> <Cmd>split<CR>", opts)
+set("n", "<C-w>k", "<Cmd>set nosplitbelow<CR> <Cmd>split<CR> <Cmd>set splitbelow<CR>", opts)
+set("n", "<C-w>l", "<Cmd>set splitright<CR> <Cmd>vsplit<CR>", opts)
 
 -- visual indents
 set("v", "<", "<gv", opts)
