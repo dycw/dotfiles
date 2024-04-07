c97d737d67925420cd83f86733ea70d6de8d9d0e
Derek Wan <d.wan@icloud.com>
2024-04-07 23:06:43 +0900

    Update `neovim` config (#47)

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -42,15 +42,21 @@ opt.foldmethod = "manual"
 -- The ":substitute" flag `'g'`?
 opt.gdefault = true
 
+-- When off a buffer is unloaded when it is abandoned?
+opt.hidden = true
+
 -- When there is a previous search pattern, highlight all its matches
 opt.hlsearch = true
 
 -- Ignore case in search patterns
 opt.ignorecase = true
 
--- When nonempty, shows the effects of |:substitute|, |:smagic|, |:snomagic| and user commands with the
--- |:command-preview| flag as you type
+-- When nonempty, shows the effects of substitute, smagic, snomagic and user commands with the command-preview flag as
+-- you type
 opt.inccommand = "split"
+
+-- The value of this option influences when the last window will have a status line
+opt.laststatus = 3
 
 -- By default, show tabs as ">", trailing spaces as "-", and non-breakable space characters as "+".
 opt.list = true
@@ -64,17 +70,26 @@ opt.mouse = "a"
 -- Print the line number in front of each line?
 opt.number = true
 
+-- Minimal number of columns to use for the line number
+opt.numberwidth = 4
+
 -- Maximum number of items to show in the popup menu
 opt.pumheight = 10
 
 -- Show the line number relative to the line with the cursor in front of each line?
 opt.relativenumber = true
 
+-- Show the line and column number of the cursor position, separated by a comma?
+opt.ruler = false
+
 -- Minimal number of screen lines to keep above and below the cursor
 opt.scrolloff = 10
 
 -- If in Insert, Replace or Visual mode put a message on the last line?
 opt.showmode = false
+
+-- The minimal number of screen columns to keep to the left and to the right of the cursor if `nowrap` is set
+opt.sidescrolloff = 10
 
 -- When and how to draw the sign column
 opt.signcolumn = "yes"
@@ -84,6 +99,9 @@ opt.smartcase = true
 
 -- Number of spaces to use for each step of (auto)indent
 opt.shiftwidth = 2
+
+-- Show command in the last line of the screen?
+opt.showcmd = false
 
 -- When on, splitting a window will put the new window right of the current one
 opt.splitright = true
@@ -97,8 +115,17 @@ opt.swapfile = false
 -- Number of spaces that a <Tab> int he file counts for
 opt.tabstop = 2
 
+-- Enables 24-bit RGB color in the TUI
+opt.termguicolors = true
+
 -- Time in milliseconds to wait for a mapped sequence to complete
 opt.timeoutlen = 300
+
+-- When on, the title of the window will be set to the value of `titlestring`
+opt.title = true
+
+-- When this option is not empty, it will be used for the title of the window
+opt.titlestring = "%<%F%=%l/%L - nvim"
 
 -- When on, Vim automatically saves undo history to an undo file when writing a buffer to a file
 opt.undofile = true
@@ -111,28 +138,3 @@ opt.wrap = true
 
 -- Make a backup before overwriting a file
 opt.writebackup = true
-
-local default_options = {
-    cmdheight = 1, -- more space in the neovim command line for displaying messages
-    foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
-    hidden = true, -- required to keep multiple buffers and open multiple buffers
-    termguicolors = true, -- set term gui colors (most terminals support this)
-    title = true, -- set the title of window to the value of the titlestring
-    -- opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
-    -- undodir = undodir, -- set an undo directory
-    writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-    expandtab = true, -- convert tabs to spaces
-    shiftwidth = 2, -- the number of spaces inserted for each indentation
-    tabstop = 2, -- insert 2 spaces for a tab
-    cursorline = true, -- highlight the current line
-    number = true, -- set numbered lines
-    numberwidth = 4, -- set number column width to 2 {default 4}
-    signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-    wrap = false, -- display lines as one long line
-    -- shadafile = join_paths(get_cache_dir(), "lvim.shada"),
-    scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor.
-    sidescrolloff = 8, -- minimal number of screen lines to keep left and right of the cursor.
-    showcmd = false,
-    ruler = false,
-    laststatus = 3,
-}
