a43ab0e8e5894b169e789d9440767e0f011052fd
Derek Wan <d.wan@icloud.com>
2024-04-10 23:43:36 +0800

    more

--- nvim/lua/custom/plugins/sort.lua
+++ nvim/lua/custom/plugins/sort.lua
@@ -2,7 +2,6 @@ return {
     "sqve/sort.nvim",
     config = function()
         local keymap_set = require("utilities").keymap_set
-
         keymap_set("n", "<Leader>so", "<Cmd>Sort<CR>", "Sort")
         keymap_set("n", "<Leader>sn", "<Cmd>Sort n<CR>", "Sort (numbers)")
         keymap_set("v", "<Leader>so", "<Esc><Cmd>Sort<CR>", "Sort")
