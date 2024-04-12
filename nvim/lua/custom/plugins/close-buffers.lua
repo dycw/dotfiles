588dedb3a1a8e184d1ea58032d2e774ec3c921dd
Derek Wan <d.wan@icloud.com>
2024-04-13 01:43:35 +0900

    Add `close-buffers` cmd (#62)

--- nvim/lua/custom/plugins/close-buffers.lua
+++ nvim/lua/custom/plugins/close-buffers.lua
@@ -1,4 +1,9 @@
 return {
     "kazhala/close-buffers.nvim",
+    config = function()
+        require("utilities").keymap_set("n", "<Leader>bd", function()
+            require("close_buffers").delete({ type = "this" })
+        end, "Buffer [D]elete")
+    end,
     event = "VeryLazy",
 }
