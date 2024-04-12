7822e3ed714b1afcc0c301805571da95b0ab2021
Derek Wan <d.wan@icloud.com>
2024-04-12 22:32:20 +0900

    Add autocmds and flutter-tools (#57)

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -3,18 +3,27 @@ local v = vim
 -- luacheck: pop
 local api = v.api
 
+api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    callback = function()
+        v.cmd("set filetype=jsonc")
+    end,
+    desc = "JSONC support for VSCode files",
+    group = api.nvim_create_augroup("jsonc-vscode", { clear = true }),
+    pattern = "*/.vscode/*.json",
+})
+
 api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
     callback = function()
         v.highlight.on_yank()
     end,
+    desc = "Highlight when yanking (copying) text",
+    group = api.nvim_create_augroup("highlight-yank", { clear = true }),
 })
 
 api.nvim_create_autocmd("VimResized", {
-    desc = "Auto resize windows when Vim is resized",
-    group = api.nvim_create_augroup("auto-resize-windows", { clear = true }),
     callback = function()
         v.cmd("wincmd =")
     end,
+    desc = "Auto resize windows when Vim is resized",
+    group = api.nvim_create_augroup("auto-resize-windows", { clear = true }),
 })
---
+++ nvim/lua/custom/plugins/flutter-tools.lua
@@ -0,0 +1,9 @@
+return {
+    "akinsho/flutter-tools.nvim",
+    config = true,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    lazy = false,
+}
