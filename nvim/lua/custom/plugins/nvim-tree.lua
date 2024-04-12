3f90e7f400e2de39004bdf331e2f6be513ef8da6
Derek Wan <d.wan@icloud.com>
2024-04-13 01:02:06 +0900

    Add auto-save and nvim-tree mappings (#59)

--- nvim/lua/custom/plugins/nvim-tree.lua
+++ nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-tree/nvim-tree.lua",
     config = function()
+        require("utilities").keymap_set({ "n", "v" }, "<Leader>fe", "<Cmd>NvimTreeToggle<CR>", "File Explorer")
         require("nvim-tree").setup()
     end,
     event = "VeryLazy",
