ef3fccafc2e6e8cea930d139051706e54a27ee05
Derek Wan <d.wan@icloud.com>
2024-04-13 00:00:50 +0800

    Add auto-save and nvim-tree mappings

--- nvim/lua/custom/plugins/auto-save.lua
+++ nvim/lua/custom/plugins/auto-save.lua
@@ -1,6 +1,7 @@
 return {
     "okuuva/auto-save.nvim", -- forked from pocco81/auto-save.nvim
     config = function()
+        require("utilities").keymap_set("n", "<leader>as", "<Cmd>ASToggle<CR>", "Toggle Trouble")
         require("auto-save").setup({
             enabled = false,
         })
