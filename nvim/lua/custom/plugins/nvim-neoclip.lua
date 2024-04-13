6544c22174cbc9ca9c2658cb671b3a96535d7146
Derek Wan <d.wan@icloud.com>
2024-04-13 17:04:43 +0900

    Add `neoclip` (#66)

---
+++ nvim/lua/custom/plugins/nvim-neoclip.lua
@@ -0,0 +1,22 @@
+return {
+    "acksld/nvim-neoclip.lua",
+    config = function()
+        require("neoclip").setup({
+            enable_persistent_history = true,
+            keys = {
+                telescope = {
+                    i = {
+                        paste = "<CR>",
+                    },
+                },
+            },
+        })
+        require("utilities").keymap_set("n", "<Leader>p", function()
+            require("telescope").extensions.neoclip.neoclip()
+        end, "Neoclip")
+    end,
+    dependencies = {
+        { "nvim-telescope/telescope.nvim" },
+        { "kkharji/sqlite.lua", module = "sqlite" },
+    },
+}
