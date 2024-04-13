0c9e54b57744bb6db2e46c0aa5abf348f2d36af5
Derek Wan <d.wan@icloud.com>
2024-04-13 19:12:10 +0900

    Refactor variables in `dial.lua` (#74)

--- nvim/lua/custom/plugins/dial.lua
+++ nvim/lua/custom/plugins/dial.lua
@@ -2,12 +2,15 @@ return {
     "monaqa/dial.nvim",
     config = function()
         local keymap_set = require("utilities").keymap_set
-        keymap_set("n", "<C-a>", require("dial.map").inc_normal(), "Increment")
-        keymap_set("n", "<C-x>", require("dial.map").dec_normal(), "Decrement")
-        keymap_set("v", "<C-a>", require("dial.map").inc_visual(), "Increment")
-        keymap_set("v", "<C-x>", require("dial.map").dec_visual(), "Decrement")
-        keymap_set("v", "g<C-a>", require("dial.map").inc_gvisual(), "Increment")
-        keymap_set("v", "g<C-x>", require("dial.map").dec_gvisual(), "Decrement")
+
+        local map = require("dial.map")
+        keymap_set("n", "<C-a>", map.inc_normal(), "Increment")
+        keymap_set("n", "<C-x>", map.dec_normal(), "Decrement")
+        keymap_set("v", "<C-a>", map.inc_visual(), "Increment")
+        keymap_set("v", "<C-x>", map.dec_visual(), "Decrement")
+        keymap_set("v", "g<C-a>", map.inc_gvisual(), "Increment")
+        keymap_set("v", "g<C-x>", map.dec_gvisual(), "Decrement")
+
         local augend = require("dial.augend")
         require("dial.config").augends:register_group({
             default = {
