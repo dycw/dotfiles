27cb6ba82ee3bdfeb87b63a55f99574dc9366de3
Derek Wan <d.wan@icloud.com>
2024-04-13 19:54:48 +0900

    Set up `conform.lua` for more files (#79)

--- nvim/lua/custom/plugins/conform.lua
+++ nvim/lua/custom/plugins/conform.lua
@@ -4,23 +4,44 @@ local bo = vim.bo
 
 return {
     "stevearc/conform.nvim",
+    event = "BufWritePre",
     opts = {
-        notify_on_error = false,
+        formatters_by_ft = {
+            dart = { "dart_format" },
+            elm = { "elm_format" },
+            go = { "gofumpt" },
+            htmldjango = { "djlint", "rustywind" },
+            lua = { "stylua" },
+            python = { "ruff_fix", "ruff_format" },
+            rust = { "rustfmt" },
+            sh = { "shfmt" },
+            sql = { "sql_formatter" },
+            zig = { "zigfmt" },
+            -- prettier
+            css = { "prettier" },
+            graphql = { "prettier" },
+            json = { "prettier" },
+            less = { "prettier" },
+            markdown = { "prettier" },
+            scss = { "prettier" },
+            toml = { "prettier" },
+            vue = { "prettier" },
+            yaml = { "prettier" },
+            -- prettier + rustywind
+            html = { "prettier", "rustywind" },
+            javascript = { "prettier", "rustywind" },
+            javascriptreact = { "prettier", "rustywind" },
+            typescript = { "prettier", "rustywind" },
+            typescriptreact = { "prettier", "rustywind" },
+            -- rest
+            ["*"] = { "trim_whitespace", "trim_newlines" },
+        },
         format_on_save = function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for languages that don't
-            -- have a well standardized coding style. You can add additional
-            -- languages here or re-enable it for the disabled ones.
             local disable_filetypes = { c = true, cpp = true }
             return {
                 timeout_ms = 500,
                 lsp_fallback = not disable_filetypes[bo[bufnr].filetype],
             }
         end,
-        formatters_by_ft = {
-            javascript = { "prettier" },
-            python = { "ruff_fix", "ruff_format" },
-            lua = { "stylua" },
-            ["*"] = { "trim_whitespace", "trim_newlines" },
-        },
     },
 }
