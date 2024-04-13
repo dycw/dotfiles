4480b8351f6a6236a20c5c2f3ce0f8531057a949
Derek Wan <d.wan@icloud.com>
2024-04-13 18:14:55 +0800

    Refactor variables in `trouble.lua`

--- nvim/lua/custom/plugins/trouble.lua
+++ nvim/lua/custom/plugins/trouble.lua
@@ -2,20 +2,22 @@ return {
     "folke/trouble.nvim",
     config = function()
         local keymap_set = require("utilities").keymap_set
+        local trouble = require("trouble")
+
         keymap_set("n", "<Leader>ld", function()
-            require("trouble").open("document_diagnostics")
+            trouble.open("document_diagnostics")
         end, "Document diagnostics (list)")
         keymap_set("n", "<Leader>ll", function()
-            require("trouble").open("loclist")
+            trouble.open("loclist")
         end, "Loc list (list)")
         keymap_set("n", "<Leader>lq", function()
-            require("trouble").open("quickfix")
+            trouble.open("quickfix")
         end, "Quickfix (list)")
         keymap_set("n", "<Leader>lr", function()
-            require("trouble").open("lsp_references")
+            trouble.open("lsp_references")
         end, "References (list)")
         keymap_set("n", "<Leader>lw", function()
-            require("trouble").open("workspace_diagnostics")
+            trouble.open("workspace_diagnostics")
         end, "Workspace diagnostics (list)")
     end,
     dependencies = { "nvim-tree/nvim-web-devicons" },
