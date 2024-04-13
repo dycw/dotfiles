a871ee9a45e9cc67a5bee1e9f83a63dfa773d48a
Derek Wan <d.wan@icloud.com>
2024-04-13 18:54:42 +0900

    Add `nvim-bqf`, fix `gca` aliases, more+reorg Telescope (#72)

---
+++ nvim/lua/custom/plugins/nvim-bqf.lua
@@ -0,0 +1,7 @@
+return {
+    "kevinhwang91/nvim-bqf",
+    config = function()
+        require("bqf").setup({ auto_resize_height = true })
+    end,
+    event = "VeryLazy",
+}
--- nvim/lua/custom/plugins/nvim-tree.lua
+++ nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,7 +1,12 @@
 return {
     "nvim-tree/nvim-tree.lua",
     config = function()
-        require("utilities").keymap_set({ "n", "v" }, "<Leader>fe", "<Cmd>NvimTreeToggle<CR>", "File Explorer")
+        require("utilities").keymap_set(
+            { "n", "v" },
+            "<Leader>fe",
+            "<Cmd>NvimTreeFindFileToggle<CR>",
+            "File [E]xplorer"
+        )
         require("nvim-tree").setup()
     end,
     event = "VeryLazy",
