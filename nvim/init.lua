24a756432c7af70ed51044abbf739f6686f2fb62
Derek Wan <d.wan@icloud.com>
2024-04-14 11:39:07 +0900

    Disable `lazy` change detection (#90)

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,24 +23,12 @@ end ---@diagnostic disable-next-line: undefined-field
 v.opt.rtp:prepend(lazypath)
 
 -- plugins
-require("lazy").setup({ { import = "custom.plugins" } }, {
-    ui = {
-        -- If you are using a Nerd Font: set icons to an empty table which will use the
-        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-        icons = v.g.have_nerd_font and {} or {
-            cmd = "⌘",
-            config = "🛠",
-            event = "📅",
-            ft = "📂",
-            init = "⚙",
-            keys = "🗝",
-            plugin = "🔌",
-            runtime = "💻",
-            require = "🌙",
-            source = "📄",
-            start = "🚀",
-            task = "📌",
-            lazy = "💤 ",
-        },
+local spec = {
+    { import = "custom.plugins" },
+}
+local opts = {
+    change_detection = {
+        enabled = false,
     },
-})
+}
+require("lazy").setup(spec, opts)
