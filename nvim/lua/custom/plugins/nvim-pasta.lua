3682c3c6f5c5d8f9ef43e597cabad17bbf4ee787
Derek Wan <d.wan@icloud.com>
2024-04-16 21:56:29 +0800

    Remove `nvim-pasta`

--- nvim/lua/custom/plugins/nvim-pasta.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "hrsh7th/nvim-pasta",
-    config = function()
-        local mapping = require("pasta.mapping")
-        local keymap_set = require("utilities").keymap_set
-        keymap_set({ "n", "x" }, "p", mapping.p, "Paste after")
-        keymap_set({ "n", "x" }, "P", mapping.P, "Paste before")
-    end,
-}
