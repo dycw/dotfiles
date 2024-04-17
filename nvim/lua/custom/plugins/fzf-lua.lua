6ede85b1ff94014ca17b3af223bf40464b70b6de
Derek Wan <d.wan@icloud.com>
2024-04-18 01:04:31 +0900

    Switch to `git-files` upon opening (#123)

--- nvim/lua/custom/plugins/fzf-lua.lua
+++ nvim/lua/custom/plugins/fzf-lua.lua
@@ -72,7 +72,7 @@ return {
         api.nvim_create_autocmd("VimEnter", {
             callback = function()
                 if v.fn.argv(0) == "" then
-                    fzf_lua.oldfiles()
+                    fzf_lua.git_files()
                 end
             end,
             desc = "git-files upon entering vim",
