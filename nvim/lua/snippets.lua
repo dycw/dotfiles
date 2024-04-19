fd123fbb8173d36136e32b7d8af222974fa370ca
Derek Wan <d.wan@icloud.com>
2024-04-19 22:28:36 +0800

    Switch `gf` to use `GitFilesMRU`

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -9,7 +9,13 @@ ls.add_snippets("python", {
     s("imdt", { t("import datetime as dt") }),
     s("fu-annotations", { t("from __future__ import annotations") }),
 
+    -- dataclasses
+    s("da-dataclasses", { t("from dataclasses import dataclass") }),
+
     -- utilities
     s("ut-ensure-str", { t("from utilities.text import ensure_str") }),
+    s("ut-index-s", { t("utilities.pandas import IndexS") }),
     s("ut-one", { t("from utilities.itertools import one") }),
+    s("ut-partial", { t("from utilities.functools import partial") }),
+    s("ut-check-polars-dataframe", { t("from utilities.polars import check_polars_dataframe") }),
 })
