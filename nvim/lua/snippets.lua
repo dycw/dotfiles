5923c24652677d928a5a4f25d948d9fa6c6f1b39
Derek Wan <d.wan@icloud.com>
2024-05-28 19:56:43 +0900

    wip

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -134,7 +134,7 @@ ls.add_snippets("python", {
     s("ut-ensure-float", { t({ "from utilities.types import ensure_float", "" }) }),
     s("ut-ensure-int", { t({ "from utilities.types import ensure_int", "" }) }),
     s("ut-ensure-member", { t({ "from utilities.types import ensure_member", "" }) }),
-    s("ut-ensure-not-none", { t({ "from utilities.types import ensure_not_none", "" }) }),
+    s("ut-ensure-number", { t({ "from utilities.types import ensure_number", "" }) }),
     s("ut-ensure-number", { t({ "from utilities.types import ensure_number", "" }) }),
     s("ut-ensure-str", { t({ "from utilities.text import ensure_str", "" }) }),
     s("ut-extract-group", { t({ "from utilities.re import extract_group", "" }) }),
