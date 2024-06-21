58adbaf5ea53385f2ccf86a86fc0c01a7087b589
Derek Wan <d.wan@icloud.com>
2024-06-22 02:51:04 +0900

    Move to `JUPYTER_PLATFORM_DIRS` (#157)

--- nvim/lua/custom/plugins/flutter-tools.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "akinsho/flutter-tools.nvim",
-    config = true,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "stevearc/dressing.nvim", -- optional for vim.ui.select
-    },
-    lazy = false,
-}
