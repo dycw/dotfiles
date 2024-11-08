34c27e8ce325ac4b28e6b7cecb667d9f36b213db
Derek Wan <d.wan@icloud.com>
2024-11-08 17:55:09 +0900

    save

--- nvim/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "github/copilot.vim",
-    config = function()
-        require("copilot").setup({
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-        })
-    end,
-}
