26044db8fc6572dafed4e0f7b74cb764d0be7beb
Derek Wan <d.wan@icloud.com>
2024-11-08 17:42:12 +0900

    remove chat

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,9 @@
 return {
     "github/copilot.vim",
+    config = function()
+        require("copilot").setup({
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+        })
+    end,
 }
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -46,11 +46,11 @@ return {
                 end,
             },
             sources = {
-                { name = "copilot" },
-                { name = "buffer" },
-                { name = "luasnip" },
-                { name = "nvim_lsp" },
-                { name = "path" },
+                { name = "copilot", group_index = 2 },
+                { name = "nvim_lsp", group_index = 2 },
+                { name = "luasnip", group_index = 2 },
+                { name = "path", group_index = 2 },
+                { name = "buffer", group_index = 2 },
             },
         })
     end,
