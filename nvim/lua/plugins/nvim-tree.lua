60ed89fcb2b22f98549c0a70ae44dccb6b606d57
Derek Wan <d.wan@icloud.com>
2024-11-26 13:07:10 +0900

    save

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -7,6 +7,13 @@ return {
             "<Cmd>NvimTreeFindFileToggle<CR>",
             "File [E]xplorer"
         )
-        require("nvim-tree").setup({ git = { ignore = true } })
+        require("nvim-tree").setup({
+            filters = {
+                custom = { "__pycache__" },
+            },
+            git = {
+                ignore = true,
+            },
+        })
     end,
 }
