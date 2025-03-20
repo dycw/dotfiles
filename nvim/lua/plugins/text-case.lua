d8a6fa53cb3cc119d715cd83c129945fbc2568f4
Derek Wan <d.wan@icloud.com>
2025-03-21 08:56:01 +0900

    Saved at 2025-03-21 08:56:01 (Fri)

--- nvim/lua/plugins/text-case.lua
+++ nvim/lua/plugins/text-case.lua
@@ -6,7 +6,6 @@ return {
         require("telescope").load_extension("textcase")
 
         local keymap_set = require("utilities").keymap_set
-        keymap_set({ "n", "v" }, "<Leader>cc", "<Cmd>TextCaseOpenTelescopeQuickChange<CR>", "change [c]ase")
         keymap_set("n", "<Leader>su", "<Cmd>TextCaseStartReplacingCommand<CR>", "s[u]bstitute")
         keymap_set("v", "<Leader>su", ":Subs/", "s[u]bstitute")
     end,
