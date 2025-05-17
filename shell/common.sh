9a6e24d8ce65a4a7544296b2cf36c164aa1c3e31
Derek Wan <d.wan@icloud.com>
2025-05-17 21:58:46 +0900

    Commited by derekwan@DW-Mac at 2025-05-17 21:58:46 (Sat)

--- shell/common.sh
+++ shell/common.sh
@@ -118,7 +118,7 @@ fi
 if command -v gh >/dev/null 2>&1; then
 	ghc() {
 		if [ $# -eq 1 ]; then
-			gh pr create --title="$1 --body"
+			gh pr create --title="$1 --body=''"
 			return $?
 		elif [ $# -eq 2 ]; then
 			gh pr create --title="$1" --body="Closes #$2"
