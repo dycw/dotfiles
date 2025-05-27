097c3eed5926cae4e0630859444ec6b80a33062b
Derek Wan <d.wan@icloud.com>
2025-05-27 20:05:35 +0900

    Commited by derekwan@DW-Mac at 2025-05-27 20:05:35 (Tue)

--- git/aliases.sh
+++ git/aliases.sh
@@ -100,7 +100,9 @@ if command -v git >/dev/null 2>&1; then
 		else
 			echo "'gbd' accepts [0..1] arguments" || return 1
 		fi
-		git branch -D "${__gbd_branch}" || return $?
+		if __branch_exists "${__gbd_branch}"; then
+			git branch -D "${__gbd_branch}" || return $?
+		fi
 	}
 	gbdr() {
 		unset __gbdr_branch
