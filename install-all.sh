b941fef40f9d3648dee2621f9a81b00f575600cf
Derek Wan <d.wan@icloud.com>
2025-06-01 11:35:48 +0900

    Commited by derekwan@DW-Mac at 2025-06-01 11:35:48 (Sun)

--- install-all.sh
+++ install-all.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env sh
-model=$(system_profiler SPHardwareDataType | awk -F': ' '/Model Identifier/ {print $2}')
-if [ "$model" = "Mac14,12" ]; then
-	[ "$(scutil --get ComputerName)" = DW-Mac ] || sudo scutil --set ComputerName DW-Mac
-fi
 
+# echo
+echo_date() { echo "[$(date +'%Y-%m-%d %H:%M:%S')] $*"; }
+
+# machine-specific
 MACHINE_TYPE=$(system_profiler SPHardwareDataType | awk -F': ' '/Model Identifier/ {print $2}')
 case "$MACHINE_TYPE" in
-Mac14,12) echo "This is a Mac mini" ;;
-MacBook*) echo "This is a MacBook" ;;
-*) echo "Unknown model: $MACHINE_TYPE" ;;
+Mac14,12)
+	echo_date "Detected Mac-Mini..."
+
+	if [ "$(scutil --get ComputerName)" = 'DW-Mac' ]; then
+		echo_date 'Computer name is already set'
+	else
+		echo_date 'Setting computer name...'
+		sudo scutil --set ComputerName DW-Mac
+	fi
+
+	if [ "$(scutil --get HostName)" = 'DW-Mac' ]; then
+		echo_date 'Host name is already set'
+	else
+		echo_date 'Setting host name...'
+		sudo scutil --set HostName DW-Mac
+	fi
+	;;
+MacBook10,1) echo_date "Detected MacBook.." ;;
+*) echo_date "Unknown \$MACHINE_TYPE: ${MACHINE_TYPE}" ;;
 esac
