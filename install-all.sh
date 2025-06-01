ee5d1c7eb8f2dbfd7313215900ea71728a560b55
Derek Wan <d.wan@icloud.com>
2025-06-01 11:52:21 +0900

    Commited by derekwan@DW-Mac at 2025-06-01 11:52:21 (Sun)

--- install-all.sh
+++ install-all.sh
@@ -9,6 +9,7 @@ case "$MACHINE_TYPE" in
 Mac14,12)
 	echo_date "Detected Mac-Mini..."
 
+	# computer name
 	if [ "$(scutil --get ComputerName)" = 'DW-Mac' ]; then
 		echo_date 'Computer name is already set'
 	else
@@ -16,11 +17,28 @@ Mac14,12)
 		sudo scutil --set ComputerName DW-Mac
 	fi
 
+	# host name
 	if [ "$(scutil --get HostName)" = 'DW-Mac' ]; then
 		echo_date 'Host name is already set'
 	else
 		echo_date 'Setting host name...'
 		sudo scutil --set HostName DW-Mac
+	fi
+
+	# local host name
+	if [ "$(scutil --get LocalHostName)" = 'DW-Mac' ]; then
+		echo_date 'Local host name is already set'
+	else
+		echo_date 'Setting local host name...'
+		sudo scutil --set LocalHostName DW-Mac
+	fi
+
+	# sleep
+	if [ "$(pmset -g custom | awk '/ sleep[[:space:]]/ {print $2}')" = "0" ]; then
+		echo_date 'Sleep is already set'
+	else
+		echo_date 'Setting sleep...'
+		sudo pmset -a sleep 0
 	fi
 	;;
 MacBook10,1) echo_date "Detected MacBook.." ;;
