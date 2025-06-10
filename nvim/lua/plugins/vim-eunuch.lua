014a748482eaccf9c50cb631da3e26217b1284d2
Derek Wan <d.wan@icloud.com>
2025-06-10 10:28:22 +0900

    Commited by derekwan@DW-Mac at 2025-06-10 10:28:22 (Tue)

--- nvim/lua/plugins/vim-eunuch.lua
+++ nvim/lua/plugins/vim-eunuch.lua
@@ -1,7 +1,12 @@
-return {
+-- luacheck: push ignore
+local v = vim
+-- luacheck: pop
 
+return {
     "tpope/vim-eunuch",
     config = function()
-        require("utilities").keymap_set("n", "<Leader>rf", "<Cmd>Rename<CR>", "Rename [f]ile")
+        require("utilities").keymap_set("n", "<Leader>rf", function()
+            v.api.nvim_feedkeys(":Rename " .. v.fn.expand("%:t"), "n", false)
+        end, "Rename [f]ile")
     end,
 }
