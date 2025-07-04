faa59dd7742de8b8c2282727cf2cd6e63d89239a
Derek Wan <d.wan@icloud.com>
2025-07-04 12:31:44 +0900

    Commited by derekwan@DW-Mac at 2025-07-04 12:31:44 (Fri)

--- shell/common.sh
+++ shell/common.sh
@@ -314,21 +314,15 @@ fi
 
 # openvpn
 openvpn_home() {
-	unset __openvpn_home_count __openvpn_home_file
-	__openvpn_home_count=0
-	first=""
-	find "${HOME}/dropbox" -type f -name "*.ovpn" | while IFS= read -r __openvpn_home_file; do
-		__openvpn_home_count=$((__openvpn_home_count + 1))
-	done
-
-	if [ "$__openvpn_home_count" -eq 0 ]; then
-		echo_date "ERROR: no '*.ovpn' file found"
-	elif [ "$__openvpn_home_count" -eq 1 ]; then
-		find "$HOME/DB" -type f -name "*.ovpn"
+	__openvpn_home_file=$(find "${HOME}/Dropbox/qrt" -type f -name '*.ovpn' -print -quit)
+	if [ -f "${__openvpn_home_file}" ]; then
+		echo_date "Connecting home using ${__openvpn_home_file}..."
+		sudo openvpn --config "${__openvpn_home_file}"
 	else
-		echo_date "ERROR: multiple '*.ovpn' files found"
+		echo_date "ERROR: no '*.ovpn' file found"
 	fi
 }
+
 # path
 echo_path() {
 	if [ $# -eq 0 ]; then
