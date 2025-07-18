d7595853c91959bfd206d96d902db8273d10a8eb
Derek Wan <d.wan@icloud.com>
2025-07-18 09:27:47 +0900

    Commited by derekwan@RH-Mac at 2025-07-18 09:27:47 (Fri)

--- install-all.sh
+++ install-all.sh
@@ -186,7 +186,7 @@ brew_install luacheck
 brew_install nvim neovim
 brew_install pgadmin4 --cask
 brew_install pgcli
-[ -n "${IS_MAC}" ] && brew_install postgres@17
+[ -n "${IS_MAC}" ] && brew_install postgresql@17
 [ -n "${IS_MAC_MINI}" ] && brew_install postico --cask
 brew_install pre-commit
 brew_install prettier
