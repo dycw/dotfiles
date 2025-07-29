ea2de9a2901d9e07c7e918fd9ece98e6eaa02403
Derek Wan <d.wan@icloud.com>
2025-07-29 17:31:40 +0900

    <derekwan/RH-MacBook/20250729T173140 (Tue)>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,10 +5,24 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+config.animation_fps = 1
 config.audible_bell = "Disabled"
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.font_size = 12.0
 config.initial_cols = 200
 config.initial_rows = 100
-config.font_size = 12.0
+config.max_fps = 60
+config.window_close_confirmation = "NeverPrompt"
+
+config.keys = {
+    -- Disable Cmd+Ctrl+F (macOS native full-screen shortcut)
+    {
+        key = "f",
+        mods = "CMD|CTRL",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+}
 
 -- and finally, return the configuration to wezterm
 return config
