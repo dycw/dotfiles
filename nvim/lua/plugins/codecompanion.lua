8c0a5c6e63853f21bbcdb8ac4f855cb32cd06745
Derek Wan <d.wan@icloud.com>
2025-08-24 10:29:39 +0900

    Update aliases (#372)

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,51 @@
+-- luacheck: push ignore
+local v = vim
+-- luacheck: pop
+
+return {
+    "olimorris/codecompanion.nvim",
+    config = function()
+        local keymap_set = require("utilities").keymap_set
+        keymap_set({ "n", "v" }, "<Leader>ca", "<Cmd>CodeCompanionActions<CR>", "CodeCompanion [a]ctions")
+        keymap_set("n", "<Leader>c", "<Cmd>CodeCompanionChat Toggle<CR>", "[C]odeCompanion")
+        keymap_set("v", "<Leader>c", "<Cmd>CodeCompanion<CR>", "[C]odeCompanion")
+        keymap_set("v", "ga", "<Cmd>CodeCompanionChat Add<CR>", "CodeCompanion [a]dd")
+        v.cmd([[cab cc CodeCompanion]])
+
+        local strategy = {
+            adapter = {
+                name = "qrt_ollama",
+                model = "qwen3-coder:30b",
+                -- model = "gpt-oss:20b",
+            },
+        }
+        require("codecompanion").setup({
+            adapters = {
+                qrt_ollama = function()
+                    return require("codecompanion.adapters").extend("ollama", {
+                        env = {
+                            url = "OLLAMA_URL",
+                            api_key = "OLLAMA_API_KEY",
+                        },
+                        headers = {
+                            ["Content-Type"] = "application/json",
+                            ["Authorization"] = "Bearer ${api_key}",
+                        },
+                        parameters = {
+                            sync = true,
+                        },
+                    })
+                end,
+            },
+            strategies = {
+                chat = strategy,
+                inline = strategy,
+                cmd = strategy,
+            },
+        })
+    end,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+}
