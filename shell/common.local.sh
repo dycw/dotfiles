b4ee95655cc855f1393e1198e45a2e9ff3ee8f57
Derek Wan <d.wan@icloud.com>
2025-09-12 13:01:31 +0900

    2025-09-12 13:01:29 (Fri) > DW-Mac > derekwan

--- shell/common.local.sh
+++ shell/common.local.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-export OLLAMA_SERVER='a.b.c.d:p'
+export AI_ENGINE='lmstudio'
+export LMSTUDIO_SERVER='192.168.c.d:p'
+export OLLAMA_SERVER='192.168.c.d:p'
 export SSH_HOME_USER='...'
 export SSH_HOME_HOST='...'
 export SSH_TUNNEL_HOME_PORT='...'
