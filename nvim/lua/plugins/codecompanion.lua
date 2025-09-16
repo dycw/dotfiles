4bb5f26925109dbd39bde3509b1c8059cacdd11e
Derek Wan <d.wan@icloud.com>
2025-09-17 07:41:04 +0900

    Set up AI tools (#456)

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -57,10 +57,31 @@ return {
                 inline = strategy,
                 cmd = strategy,
             },
+            extensions = {
+                mcphub = {
+                    callback = "mcphub.extensions.codecompanion",
+                    opts = {
+                        -- MCP Tools
+                        make_tools = true,
+                        show_server_tools_in_chat = true,
+                        add_mcp_prefix_to_tool_names = false,
+                        show_result_in_chat = true,
+                        format_tool = nil,
+                        -- MCP Resources
+                        make_vars = true,
+                        -- MCP Prompts
+                        make_slash_commands = true,
+                    },
+                },
+                history = {
+                    enabled = true,
+                },
+            },
         })
     end,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
+        "ravitemer/codecompanion-history.nvim",
     },
 }
