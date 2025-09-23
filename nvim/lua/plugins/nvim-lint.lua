306e2fd49a3b28581e8681ccea9a60b897dad5b0
Derek Wan <d.wan@icloud.com>
2025-09-23 23:40:54 +0900

    2025-09-23 23:40:52 (Tue) > DW-Swift > derek

---
+++ nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,13 @@
+return {
+    "mfussenegger/nvim-lint",
+    opts = {
+        linters_by_ft = {
+            haskell = { "hlint" },
+            lua = { "luacheck" },
+            python = { "ruff" },
+            sh = { "shellcheck" },
+            sql = { "sqlfluff" },
+            zsh = { "shellcheck" },
+        },
+    },
+}
