c3ac26f4b20a54d4ddd3b84d2d26fcf73de1b856
Derek Wan <d.wan@icloud.com>
2025-09-23 23:49:03 +0900

    2025-09-23 23:49:03 (Tue) > DW-Swift > derek

--- nvim/lua/plugins/auto-save.nvim.lua
+++ nvim/lua/plugins/auto-save.nvim.lua
@@ -1,9 +1,13 @@
+-- luacheck: push ignore
+local v = vim
+-- luacheck: pop
+
 return {
     "okuuva/auto-save.nvim", -- forked from pocco81/auto-save.nvim
     config = function()
-        vim.keymap.set("n", "<leader>as", function()
+        v.keymap.set("n", "<leader>as", function()
             require("auto-save").toggle()
-        end)
+        end, { desc = "auto [s]ave" })
         require("auto-save").setup({
             enabled = true,
             debounce_delay = 10,
