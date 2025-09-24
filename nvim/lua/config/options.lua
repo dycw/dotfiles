703f8ad334f27894be70f258d902b0ac5aa41650
Derek Wan <d.wan@icloud.com>
2025-09-24 19:53:14 +0900

    2025-09-24 19:53:13 (Wed) > DW-Mac > derekwan

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,7 @@ local v = vim
 -- luacheck: pop
 
 v.g.lazyvim_mini_snippets_in_completion = true
+v.opt.backupdir = "~/.local/state/nvim/backup"
 v.opt.gdefault = true
 v.opt.swapfile = false
 v.opt.wrap = true
