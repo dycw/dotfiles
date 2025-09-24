114188d5e8035193b74bf3c5f74ef2d291539352
Derek Wan <d.wan@icloud.com>
2025-09-24 09:22:56 +0900

    2025-09-24 09:22:55 (Wed) > DW-Mac > derekwan

--- nvim/lua2/snippets.lua
+++ nvim/lua2/snippets.lua
@@ -10,17 +10,6 @@ ls.add_snippets("python", {
     s("ex-n801", { t({ "(Exception): ...  # noqa: N801", "" }) }),
 
     -- pytest
-    s("pyi", {
-        t({ "from hypothesis import HealthCheck, Phase, given, reproduce_failure, settings", "" }),
-        t({ "from pytest import RaisesGroup, approx, fixture, mark, param, raises", "" }),
-        t({ "from utilities.contextvars import set_global_breakpoint", "" }),
-    }),
-    s("py-approx", { t({ "from pytest import approx", "" }) }),
-    s("py-fixture", { t({ "from pytest import fixture", "" }) }),
-    s("py-mark", { t({ "from pytest import mark", "" }) }),
-    s("py-param", { t({ "from pytest import param", "" }) }),
-    s("py-raises", { t({ "from pytest import raises", "" }) }),
-    s("py-raises-group", { t({ "from pytest import RaisesGroup", "" }) }),
     s("py-read-pickle", {
         t({
             "from utilities.pathlib import get_root",
@@ -33,7 +22,6 @@ ls.add_snippets("python", {
             "",
         }),
     }),
-    s("py-skip", { t({ "from pytest import skip", "" }) }),
     s("py-write-pickle", {
         t({
             "from utilities.pathlib import get_root",
@@ -47,27 +35,6 @@ ls.add_snippets("python", {
             "",
         }),
     }),
-    s("with-raises", {
-        t("with raises("),
-        i(1, "Exception"),
-        t(', match="'),
-        i(2, "match"),
-        t('") as error:'),
-        t(""),
-        t("    pass"),
-    }),
-
-    -- pytest-benchmark
-    s("py-benchmark-fixture", { t({ "from pytest_benchmark.fixture import BenchmarkFixture", "" }) }),
-
-    -- pytest-lazy-fixtures
-    s("py-lf", { t({ "from pytest_lazy_fixtures import lf", "" }) }),
-
-    -- pytest-regressions
-    s(
-        "py-dataframe-regression-fixture",
-        { t({ "from pytest_regressions.dataframe_regression import DataFrameRegressionFixture", "" }) }
-    ),
 
     -- typing
     s("case-never", {
