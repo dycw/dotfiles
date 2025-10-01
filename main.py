c9e09894bcea3b6c338c0be795ae080e0293616e
Derek Wan <d.wan@icloud.com>
2025-10-01 19:17:40 +0900

    Add `main.py` to setup Debian (#484)

---
+++ main.py
@@ -0,0 +1,513 @@
+#!/usr/bin/env python3
+# ruff: noqa: C901,E501,S310,PLR0912,S602,S607
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from collections.abc import Iterator
+from contextlib import contextmanager
+from dataclasses import dataclass
+from logging import basicConfig, getLogger
+from pathlib import Path
+from shutil import copyfile, which
+from stat import S_IXUSR
+from string import Template
+from subprocess import check_call, check_output
+from tempfile import TemporaryDirectory
+from urllib.parse import urlparse
+from urllib.request import urlopen
+
+# constants
+
+
+_LOGGER = getLogger(__name__)
+
+
+# settings
+
+
+@dataclass(order=True, unsafe_hash=True, kw_only=True)
+class _Settings:
+    age: bool
+    bat: bool
+    bottom: bool
+    build_essential: bool
+    delta: bool
+    direnv: bool
+    fd_find: bool
+    jq: bool
+    just: bool
+    ripgrep: bool
+    shellcheck: bool
+    shfmt: bool
+    sops: bool
+    tmux: bool
+    verbose: bool
+    yq: bool
+    zoom: bool
+
+    @classmethod
+    def parse(cls) -> "_Settings":
+        parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+        parser.add_argument("-a", "--age", action="store_true", help="Install 'age'.")
+        parser.add_argument("-ba", "--bat", action="store_true", help="Install 'bat'.")
+        parser.add_argument(
+            "-bt", "--bottom", action="store_true", help="Install 'bottom'."
+        )
+        parser.add_argument(
+            "-bu",
+            "--build-essential",
+            action="store_true",
+            help="Install 'build-essential'.",
+        )
+        parser.add_argument(
+            "-de", "--delta", action="store_true", help="Install 'delta'."
+        )
+        parser.add_argument(
+            "-di", "--direnv", action="store_true", help="Install 'direnv'."
+        )
+        parser.add_argument(
+            "-f", "--fd-find", action="store_true", help="Install 'fd-find'."
+        )
+        parser.add_argument(
+            "-ju", "--just", action="store_true", help="Install 'just'."
+        )
+        parser.add_argument("-jq", "--jq", action="store_true", help="Install 'jq'.")
+        parser.add_argument(
+            "-r", "--ripgrep", action="store_true", help="Install 'ripgrep'."
+        )
+        parser.add_argument(
+            "-sc", "--shellcheck", action="store_true", help="Install 'shellcheck'."
+        )
+        parser.add_argument(
+            "-sf", "--shfmt", action="store_true", help="Install 'shfmt'."
+        )
+        parser.add_argument(
+            "-so", "--sops", action="store_true", help="Install 'sops'."
+        )
+        parser.add_argument("-t", "--tmux", action="store_true", help="Install 'tmux'.")
+        parser.add_argument("-yq", "--yq", action="store_true", help="Install 'yq'.")
+        parser.add_argument("-z", "--zoom", action="store_true", help="Install 'zoom'.")
+        parser.add_argument(
+            "-v", "--verbose", action="store_true", help="Verbose mode."
+        )
+        return _Settings(**vars(parser.parse_args()))
+
+
+# script
+
+
+def main(settings: _Settings, /) -> None:
+    basicConfig(
+        format="{asctime} | {levelname:8} | {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{",
+        level="DEBUG" if settings.verbose else "INFO",
+    )
+    _setup_shells()
+    _install_curl()
+    _install_git()
+    _install_neovim()
+    _install_starship()
+    if settings.age:
+        _install_age()
+    if settings.bat:
+        _install_bat()
+    if settings.bottom:
+        _install_bottom()
+    if settings.build_essential:
+        _install_build_essential()
+    if settings.delta:
+        _install_delta()
+    if settings.direnv:
+        _install_direnv()
+    if settings.fd_find:
+        _install_fd_find()
+    if settings.just:
+        _install_just()
+    if settings.jq:
+        _install_jq()
+    if settings.ripgrep:
+        _install_ripgrep()
+    if settings.shellcheck:
+        _install_shellcheck()
+    if settings.shfmt:
+        _install_shfmt()
+    if settings.sops:
+        _install_sops()
+    if settings.tmux:
+        _install_tmux()
+    if settings.yq:
+        _install_yq()
+    if settings.zoom:
+        _install_zoom()
+
+    _install_uv()  # after curl
+
+    _install_bump_my_version()  # after uv
+    _install_pre_commit()  # after uv
+    _install_pyright()  # after uv
+    _install_ruff()  # after uv
+
+
+def _install_age() -> None:
+    if which("age"):
+        _LOGGER.debug("'age' is already installed")
+        return
+    _LOGGER.info("Installing 'age'...")
+    _apt_install("age")
+
+
+def _install_bat() -> None:
+    if which("batcat"):
+        _LOGGER.debug("'bat' is already installed")
+        return
+    _LOGGER.info("Installing 'bat'...")
+    _apt_install("bat")
+
+
+def _install_bottom() -> None:
+    if which("btm"):
+        _LOGGER.debug("'btm' is already installed")
+        return
+    _LOGGER.info("Installing 'bottom'...")
+    with _yield_github_latest_download(
+        "ClementTsang", "bottom", "bottom_${tag}-1_amd64.deb"
+    ) as path:
+        _dpkg_install(path)
+
+
+def _install_build_essential() -> None:
+    if which("cc"):
+        _LOGGER.debug(
+            "'cc' is already installed (already presumable so is 'build-essential'"
+        )
+        return
+    _LOGGER.info("Installing 'build-essential'...")
+    _apt_install("build-essential")
+
+
+def _install_bump_my_version() -> None:
+    _uv_tool_install("bump-my-version")
+
+
+def _install_curl() -> None:
+    if which("curl"):
+        _LOGGER.debug("'curl' is already installed")
+        return
+    _LOGGER.info("Installing 'curl'...")
+    _apt_install("curl")
+
+
+def _install_delta() -> None:
+    if which("delta"):
+        _LOGGER.debug("'git-delta' is already installed")
+        return
+    _LOGGER.info("Installing 'git-delta'...")
+    _apt_install("git-delta")
+
+
+def _install_direnv() -> None:
+    if which("direnv"):
+        _LOGGER.debug("'direnv' is already installed")
+    else:
+        _LOGGER.info("Installing 'direnv'...")
+        _apt_install("direnv")
+    _append_to_file("~/.direnv", '''eval "$(direnv hook bash)"''')
+
+
+def _install_fd_find() -> None:
+    if which("fdfind"):
+        _LOGGER.debug("'fd-find' is already installed")
+        return
+    _LOGGER.info("Installing 'fd-find'...")
+    _apt_install("fd-find")
+
+
+def _install_git() -> None:
+    if which("git"):
+        _LOGGER.debug("'git' is already installed")
+    else:
+        _LOGGER.info("Installing 'git'...")
+        _apt_install("git")
+    for filename in ["config", "ignore"]:
+        _setup_symlink(
+            f"~/.config/git/{filename}", f"{_get_script_dir()}/git/{filename}"
+        )
+
+
+def _install_just() -> None:
+    if which("just"):
+        _LOGGER.debug("'just' is already installed")
+        return
+    _LOGGER.info("Installing 'just'...")
+    _apt_install("just")
+
+
+def _install_jq() -> None:
+    if which("jq"):
+        _LOGGER.debug("'jq' is already installed")
+        return
+    _LOGGER.info("Installing 'jq'...")
+    _apt_install("jq")
+
+
+def _install_neovim() -> None:
+    if which("nvim"):
+        _LOGGER.debug("'neovim' is already installed")
+        return
+    _LOGGER.info("Installing 'neovim'...")
+    _apt_install("neovim")
+
+
+def _install_pre_commit() -> None:
+    _uv_tool_install("pre-commit")
+
+
+def _install_pyright() -> None:
+    _uv_tool_install("pyright")
+
+
+def _install_ruff() -> None:
+    _uv_tool_install("ruff")
+
+
+def _install_ripgrep() -> None:
+    if which("rg"):
+        _LOGGER.debug("'ripgrep' is already installed")
+        return
+    _LOGGER.info("Installing 'ripgrep'...")
+    _apt_install("ripgrep")
+
+
+def _install_shellcheck() -> None:
+    if which("shellcheck"):
+        _LOGGER.debug("'shellcheck' is already installed")
+        return
+    _LOGGER.info("Installing 'shellcheck'...")
+    _apt_install("shellcheck")
+
+
+def _install_shfmt() -> None:
+    if which("shfmt"):
+        _LOGGER.debug("'shfmt' is already installed")
+        return
+    _LOGGER.info("Installing 'shfmt'...")
+    _apt_install("shfmt")
+
+
+def _install_sops() -> None:
+    if which("sops"):
+        _LOGGER.debug("'sops' is already installed")
+        return
+    _LOGGER.info("Installing 'sops'...")
+    path_to = _get_local_bin().joinpath("sops")
+    path_to.parent.mkdir(parents=True, exist_ok=True)
+    with _yield_github_latest_download(
+        "getsops", "sops", "sops-${tag}.linux.amd64"
+    ) as binary:
+        _copyfile(binary, path_to, executable=True)
+
+
+def _install_starship() -> None:
+    if which("starship"):
+        _LOGGER.debug("'starship' is already installed")
+    else:
+        _LOGGER.info("Installing 'starship'...")
+        _apt_install("starship")
+    _setup_symlink("~/.config/starship", f"{_get_script_dir()}/starship/starship.toml")
+    _append_to_file("~/.bashrc", '''eval "$(starship init bash)"''')
+
+
+def _install_tmux() -> None:
+    if which("tmux"):
+        _LOGGER.debug("'tmux' is already installed")
+    else:
+        _LOGGER.info("Installing 'tmux'...")
+        _apt_install("tmux")
+    _update_submodules()
+    for filename_from, filename_to in [
+        ("tmux.conf.local", "tmux.conf.local"),
+        ("tmux.conf", ".tmux/.tmux.conf"),
+    ]:
+        _setup_symlink(
+            f"~/.config/tmux/{filename_from}", f"{_get_script_dir()}/tmux/{filename_to}"
+        )
+
+
+def _install_uv() -> None:
+    if which("uv"):
+        _LOGGER.debug("'uv' is already installed")
+        return
+    _install_curl()
+    _LOGGER.info("Installing 'uv'...")
+    check_call("curl -LsSf https://astral.sh/uv/install.sh | sh", shell=True)
+    _append_to_file(
+        "~/.bashrc", '''export PATH="${HOME}/.local/bin${PATH:+:${PATH}}"'''
+    )
+    check_call("source ~/.bashrc", shell=True)
+
+
+def _install_yq() -> None:
+    if which("yq"):
+        _LOGGER.debug("'yq' is already installed")
+        return
+    _LOGGER.info("Installing 'yq'...")
+    _apt_install("yq")
+
+
+def _install_zoom() -> None:
+    if which("zoom"):
+        _LOGGER.debug("'zoom' is already installed")
+        return
+    _LOGGER.info("Installing 'zoom'...")
+    _apt_install(
+        "libxcb-xinerama0",
+        "libxcb-xtest0",
+        "libxcb-cursor0",
+    )
+    _dpkg_install("zoom_amd64.deb")
+
+
+def _setup_bash() -> None:
+    for filename in ["bashrc", "bash_profile"]:
+        _setup_symlink(f"~/.{filename}", f"{_get_script_dir()}/bash/{filename}")
+
+
+def _setup_shells() -> None:
+    _setup_bash()
+
+
+# utilities
+
+
+def _append_to_file(path: Path | str, line: str, /) -> None:
+    path = _to_path(path)
+    try:
+        lines = path.read_text().splitlines()
+    except FileNotFoundError:
+        _LOGGER.info("Writing %r to %r...", line, str(path))
+        with path.open(mode="w") as fh:
+            _ = fh.write(f"{line}\n")
+        return
+    if any(line_i == line for line_i in lines):
+        _LOGGER.debug("%r is already in %r", line, str(path))
+        return
+    _LOGGER.info("Appending %r to %r...", line, str(path))
+    with path.open(mode="a") as fh:
+        _ = fh.write(f"{line}\n")
+
+
+def _apt_install(*packages: str) -> None:
+    _LOGGER.info("Updating 'apt'...")
+    check_call("sudo apt -y update", shell=True)
+    desc = ", ".join(map(repr, packages))
+    _LOGGER.info("Installing %s...", desc)
+    joined = " ".join(packages)
+    check_call(f"sudo apt -y install {joined}", shell=True)
+
+
+def _copyfile(path_from: Path, path_to: Path, /, *, executable: bool = False) -> None:
+    _unlink(path_to)
+    _LOGGER.info("Copying %r -> %r...", str(path_from), str(path_to))
+    path_to.parent.mkdir(parents=True, exist_ok=True)
+    copyfile(path_from, path_to)
+    if executable:
+        _set_executable(path_to)
+
+
+def _dpkg_install(path: Path | str, /) -> None:
+    check_call(f"sudo dpkg -i {path}", shell=True)
+
+
+def _get_latest_tag(owner: str, repo: str, /) -> str:
+    _install_curl()
+    _install_jq()
+    return check_output(
+        f'curl -s https://api.github.com/repos/{owner}/{repo}/releases/latest | jq -r ".tag_name"',
+        shell=True,
+        text=True,
+    ).strip("\n")
+
+
+def _get_local_bin() -> Path:
+    return _to_path("~/.local/bin")
+
+
+def _get_script_dir() -> Path:
+    return Path(__file__).parent
+
+
+def _set_executable(path: Path | str, /) -> None:
+    path = _to_path(path)
+    mode = path.stat().st_mode
+    if mode & S_IXUSR:
+        _LOGGER.debug("%r is already executable", str(path))
+        return
+    _LOGGER.info("Making %r executable...", str(path))
+    path.chmod(mode | S_IXUSR)
+
+
+def _setup_symlink(path_from: Path | str, path_to: Path | str, /) -> None:
+    path_from, path_to = map(_to_path, [path_from, path_to])
+    if path_from.is_symlink and (path_from.resolve() == path_to.resolve()):
+        _LOGGER.debug("%r -> %r already symlinked", str(path_from), str(path_to))
+        return
+    path_from.parent.mkdir(parents=True, exist_ok=True)
+    _unlink(path_from)
+    _LOGGER.info("Symlinking %r -> %r", str(path_from), str(path_to))
+    path_from.symlink_to(path_to)
+
+
+def _to_path(path: Path | str, /) -> Path:
+    return Path(path).expanduser()
+
+
+def _unlink(path: Path | str, /) -> None:
+    path = _to_path(path)
+    if path.exists() or path.is_symlink():
+        _LOGGER.info("Removing %r...", str(path))
+        path.unlink(missing_ok=True)
+
+
+def _update_submodules() -> None:
+    _LOGGER.info(
+        "Updating submodules...",
+    )
+    check_call("git submodule update", shell=True)
+
+
+def _uv_tool_install(tool: str, /) -> None:
+    if which(tool):
+        _LOGGER.debug("%r is already installed", tool)
+        return
+    _install_uv()
+    _LOGGER.info("Installing %r...", tool)
+    check_call(f"uv tool install {tool}", shell=True)
+
+
+@contextmanager
+def _yield_download(url: str, /) -> Iterator[Path]:
+    filename = Path(urlparse(url).path).name
+    with TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir, filename)
+        with urlopen(url) as response, temp_file.open(mode="wb") as fh:
+            fh.write(response.read())
+        yield temp_file
+
+
+@contextmanager
+def _yield_github_latest_download(
+    owner: str, repo: str, filename_template: str, /
+) -> Iterator[Path]:
+    tag = _get_latest_tag(owner, repo)
+    filename = Template(filename_template).substitute(tag=tag)
+    url = f"https://github.com/{owner}/{repo}/releases/download/{tag}/{filename}"
+    with TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir, filename)
+        with urlopen(url) as response, temp_file.open(mode="wb") as fh:
+            fh.write(response.read())
+        yield temp_file
+
+
+# main
+
+
+main(_Settings.parse())
