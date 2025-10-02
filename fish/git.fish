53c511fe0acaf96fb131a0072be78c824218c171
Derek Wan <d.wan@icloud.com>
2025-10-02 22:33:58 +0900

    More work on `git` (#489)

--- fish/git.fish
+++ fish/git.fish
@@ -24,10 +24,40 @@ if status --is-interactive; and type -q git
     function gb
         git branch --all --list --sort=-committerdate --verbose $argv
     end
+    function gbd
+        if test (count $argv) -eq 0
+            __git_branch_fzf_local | while read branch
+                __git_branch_delete $branch
+            end
+        else
+            for branch in $argv
+                __git_branch_delete $branch
+            end
+        end
+    end
+    function gbdr
+        __git_fetch_and_purge; or echo $status
+        if test (count $argv) -eq 0
+            __git_branch_fzf_remote --multi | xargs -r -I{} git push --delete origin "{}"
+        else
+            git push --delete origin $argv
+        end
+    end
     function __git_branch_delete
         git branch --delete --force $argv
     end
-
+    function __git_branch_fzf_local
+        git branch --format=%(refname:short) | fzf --multi
+    end
+    function __git_branch_fzf_remote
+        argparse m/multi -- $argv; or return $status
+        set -l args
+        if test -n "$_flag_multi"
+            set args $args --multi
+        end
+        git branch --color=never --remotes | awk '!/->/' | fzf $args \
+            | sed -E 's|^[[:space:]]*origin/||'
+    end
     # checkout
     function gco
         __git_checkout $argv
@@ -73,6 +103,23 @@ if status --is-interactive; and type -q git
             set args $args --no-verify
         end
         git commit --message="'$message'" $args; or return $status
+    end
+    function __git_commit_until
+        argparse m/message= n/no-verify -- $argv; or return $status
+        set -l args
+        if test -n "$_flag_message"
+            set args $args --message $_flag_message
+        end
+        if test -n "$_flag_no_verify"
+            set args $args --no-verify
+        end
+        for i in (seq 0 4) # @fish-lsp-disable
+            ga $argv; or return $status
+            if __git_commit $args
+                return 0
+            end
+        end
+        return 1
     end
 
     # diff
@@ -128,52 +175,52 @@ if status --is-interactive; and type -q git
         __git_push
     end
     function gpf
-        __git_push -f
+        __git_push --force
     end
     function gpn
-        __git_push -n
+        __git_push --no-verify
     end
     function gpfn
-        __git_push -f -n
+        __git_push --force --no-verify
     end
     function gpw
-        __git_push -a=web
+        __git_push --web
     end
     function gpfw
-        __git_push -f -a=web
+        __git_push --force --web
     end
     function gpnw
-        __git_push -n -a=web
+        __git_push --no-verify --web
     end
     function gpfnw
-        __git_push -f -n -a=web
+        __git_push --force --no-verify --web
     end
     function gpe
-        __git_push -a=exit
+        __git_push --exit
     end
     function gpfe
-        __git_push -f -a=exit
+        __git_push --force --exit
     end
     function gpne
-        __git_push -n -a=exit
+        __git_push --no-verify --exit
     end
     function gpfne
-        __git_push -f -n -a=exit
+        __git_push --force --no-verify --exit
     end
     function gpx
-        __git_push -a=web+exit
+        __git_push --web --exit
     end
     function gpfx
-        __git_push -f -a=web+exit
+        __git_push --force --web --exit
     end
     function gpnx
-        __git_push -n -a=web+exit
+        __git_push --no-verify --web --exit
     end
     function gpfnx
-        __git_push -f -n -a=web+exit
+        __git_push --force --no-verify --web --exit
     end
     function __git_push
-        argparse f/force n/no-verify a/action= -- $argv; or return $status
+        argparse f/force n/no-verify w/web e/exit -- $argv; or return $status
         set -l args
         if test -n "$_flag_force"
             set args $args --force
@@ -183,13 +230,11 @@ if status --is-interactive; and type -q git
             set args $args --no-verify
         end
         git push $args; or return $status
-        switch $_flag_action
-            case web
-                gitweb
-            case exit
-                exit
-            case web+exit
-                gitweb; and exit
+        if test -n "$_flag_web"
+            gitweb; or return $status
+        end
+        if test -n "$_flag_exit"
+            exit
         end
     end
 
@@ -265,6 +310,154 @@ if status --is-interactive; and type -q git
     end
 
     # combined
+    function gc
+        __git_all $argv
+    end
+    function gcf
+        __git_all $argv --force
+    end
+    function gcn
+        __git_all $argv --no-verify
+    end
+    function gcfn
+        __git_all $argv --force --no-verify
+    end
+    function gcw
+        __git_all $argv --web
+    end
+    function gcfw
+        __git_all $argv --force --web
+    end
+    function gcnw
+        __git_all $argv --no-verify --web
+    end
+    function gcfnw
+        __git_all $argv --force --no-verify --web
+    end
+    function gce
+        __git_all $argv --exit
+    end
+    function gcfe
+        __git_all $argv --force --exit
+    end
+    function gcne
+        __git_all $argv --no-verify --exit
+    end
+    function gcfne
+        __git_all $argv --force --no-verify --exit
+    end
+    function gcm
+        __git_all $argv --merge
+    end
+    function gcfm
+        __git_all $argv --force --merge
+    end
+    function gcnm
+        __git_all $argv --no-verify --merge
+    end
+    function gcfnm
+        __git_all $argv --force --no-verify --merge
+    end
+    function gcx
+        __git_all $argv --merge --exit
+    end
+    function gcfx
+        __git_all $argv --force --merge --exit
+    end
+    function gcnx
+        __git_all $argv --no-verify --merge --exit
+    end
+    function gcfnx
+        __git_all $argv --force --no-verify --merge --exit
+    end
+    function gac
+        __git_all $argv --add
+    end
+    function gacf
+        __git_all $argv --add --force
+    end
+    function gacn
+        __git_all $argv --add --no-verify
+    end
+    function gacfn
+        __git_all $argv --add --force --no-verify
+    end
+    function gacw
+        __git_all $argv --add --web
+    end
+    function gacfw
+        __git_all $argv --add --force --web
+    end
+    function gacnw
+        __git_all $argv --add --no-verify --web
+    end
+    function gacfnw
+        __git_all $argv --add --force --no-verify --web
+    end
+    function gace
+        __git_all $argv --add --exit
+    end
+    function gacfe
+        __git_all $argv --add --force --exit
+    end
+    function gacne
+        __git_all $argv --add --no-verify --exit
+    end
+    function gacfne
+        __git_all $argv --add --force --no-verify --exit
+    end
+    function gacm
+        __git_all $argv --add --merge
+    end
+    function gacfm
+        __git_all $argv --add --force --merge
+    end
+    function gacnm
+        __git_all $argv --add --no-verify --merge
+    end
+    function gacfnm
+        __git_all $argv --add --force --no-verify --merge
+    end
+    function gacx
+        __git_all $argv --add --merge --exit
+    end
+    function gacfx
+        __git_all $argv --add --force --merge --exit
+    end
+    function gacnx
+        __git_all $argv --add --no-verify --merge --exit
+    end
+    function gacfnx
+        __git_all $argv --add --force --no-verify --merge --exit
+    end
+    function __git_all
+        argparse a/add n/no-verify f/force w/web e/exit m/merge -- $argv; or return $status
+        if test -n "$_flag_add"
+            ga $argv; or return $status
+        end
+        set -l commit_args
+        if test -n "$_flag_no_verify"
+            set commit_args $commit_args --no-verify
+        end
+        __git_commit_until $commit_args; or return $status
+        set -l push_args
+        if test -n "$_flag_force"
+            set push_args $push_args --force
+        end
+        if test -n "$_flag_no_verify"
+            set push_args $push_args --no-verify
+        end
+        if test -n "$_flag_web"
+            set push_args $push_args --web
+        end
+        if test -n "$_flag_exit"; and test -z "$_flag_merge"
+            set push_args $push_args --exit
+        end
+        __git_push $push_args; or return $status
+        if test -n "$_flag_merge"
+            __github_or_gitlab_merge --delete --exit
+        end
+    end
     function __git_create_and_push
         argparse t/title= b/body= n/num= -- $argv; or return $status
         __git_fetch_and_purge; or return $status
