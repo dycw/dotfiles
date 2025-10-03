85d33cba3eb08dd92d922b7362047329b002dd9d
Derek Wan <d.wan@icloud.com>
2025-10-04 08:58:24 +0900

    '2025-10-04 08:58:24 (Sat)  > DW-Mac  > derekwan'

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,25 +1,29 @@
+-- luacheck: push ignore
+local v = vim
+-- luacheck: pop
+
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+local lazypath = v.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (v.uv or v.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
+    local out = v.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if v.v.shell_error ~= 0 then
+        v.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
             { out, "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
-        vim.fn.getchar()
+        v.fn.getchar()
         os.exit(1)
     end
 end
-vim.opt.rtp:prepend(lazypath)
+v.opt.rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+v.g.mapleader = " "
+v.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -33,3 +37,5 @@ require("lazy").setup({
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
+
+v.keymap.set("n", "<Leader>lu", "<Cmd>Lazy update<CR>", { desc = "Lazy [u]pdate" })
--- nvim/lua/plugins/bufferline.nvim.lua
+++ nvim/lua/plugins/bufferline.nvim.lua
@@ -1,16 +1,17 @@
 return {
     "akinsho/bufferline.nvim",
-    config = function()
-        require("bufferline").setup()
-    end,
     dependencies = "nvim-tree/nvim-web-devicons",
     keys = {
         { "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Buffer next" },
         { "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Buffer prev" },
     },
     opts = {
-
-        always_show_bufferline = true,
+        options = {
+            always_show_bufferline = true,
+            show_buffer_icons = false,
+            show_buffer_close_icons = false,
+            show_close_icon = false,
+        },
     },
     version = "*",
 }
