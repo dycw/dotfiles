1f2b8e57ac9472a8e38d95515cac141fa79fd88f
Derek Wan <d.wan@icloud.com>
2025-10-03 21:29:40 +0900

    '2025-10-03 21:29:40 (Fri)  > DW-Swift  > derek'

--- install.py
+++ install.py
@@ -400,10 +400,10 @@ def _install_docker() -> None:
     _run_commands(*(f"sudo apt-get remove {p}" for p in packages))
     _apt_install("ca-certificates", "curl")
     _run_commands(
-        "install -m 0755 -d /etc/apt/keyrings",
-        "curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
-        "chmod a+r /etc/apt/keyrings/docker.asc",
-        'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null',
+        "sudo install -m 0755 -d /etc/apt/keyrings",
+        "sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
+        "sudo chmod a+r /etc/apt/keyrings/docker.asc",
+        'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
     )
     _apt_install(
         "docker-ce",
@@ -412,7 +412,7 @@ def _install_docker() -> None:
         "docker-buildx-plugin",
         "docker-compose-plugin",
     )
-    _run_commands("usermod -aG docker $USER")
+    _run_commands("sudo usermod -aG docker $USER")
 
 
 def _install_dust() -> None:
