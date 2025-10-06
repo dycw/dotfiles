5096e92e9c1daf6b6002b0ff3649b47fe3a56c47
Derek Wan <d.wan@icloud.com>
2025-10-06 14:36:33 +0900

    '2025-10-06 14:36:33 (Mon)  >  DW-Swift  >  derek'

--- foo.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-if (command -v gh >/dev/null 2>&1) && (git remote get-url origin 2>/dev/null | grep -q github); then
-	printf "\n==== github ==================================================================="
-	gh pr status
-fi
