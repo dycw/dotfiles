2de617add8d23ec4c1ccf1960641b3940092f10a
Derek Wan <d.wan@icloud.com>
2025-10-06 10:26:57 +0900

    '2025-10-06 10:26:57 (Mon)  >  DW-Swift  >  derek'

--- fish/work.fish
+++ fish/work.fish
@@ -10,12 +10,7 @@ if test (hostname) = DW-Mac
         cd $HOME/work-gitlab
     end
 end
-
-# work
-if test -f $HOME/work/infra/shell/fish.fish
-    source $HOME/work/infra/shell/fish.fish
-end
-
+# git
 if type -q git
     function gclw
         if test (count $argv) -eq 0
@@ -28,4 +23,34 @@ if type -q git
         __git_clone --repo ssh://git@gitlab.main:2424/qrt/$argv[1] $args
     end
 
+end
+
+# mount
+function mount-drives
+    if test (count $argv) -eq 0
+        echo "'mount-drives' expected [1..) arguments SUBNET; got $(count $argv)" >&2; and return 1
+    end
+    set -l subnet $argv[1]
+    set -l dir /mnt/qrt-$subnet
+    set -l pool_name
+    set -l dataset_name
+    switch $subnet
+        case main
+            set pool_name mypool
+            set dataset_name mydataset
+        case test
+            set pool_name pool-test
+            set dataset_name dataset-test
+        case '*'
+            # default case
+            echo "Invalid subnet; got '$subnet'" >&2; and return 1
+    end
+    sudo apt -y install nfs-common
+    sudo mkdir -p $dir
+    sudo mount -t nfs truenas.$subnet:/mnt/$pool_name/$dataset_name $dir
+end
+
+# work
+if test -f $HOME/work/infra/shell/fish.fish
+    source $HOME/work/infra/shell/fish.fish
 end
