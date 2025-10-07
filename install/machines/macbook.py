4dd2723b64dccb3ea468f1904e8bd6a9ee049dde
Derek Wan <d.wan@icloud.com>
2025-10-07 17:25:10 +0900

    '2025-10-07 17:25:10 (Tue)  >  RH-Mac  >  derekwan'

--- install/machines/macbook.py
+++ install/machines/macbook.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-from logging import getLogger
+from logging import basicConfig, getLogger
 
 _LOGGER = getLogger(__name__)
 
 
-def setup_macbook() -> None:
-    _LOGGER.info("Setting up Macbook remotely...")
+def _main() -> None:
+    _LOGGER.info("Setting up Macbook...")
+
+
+if __name__ == "__main__":
+    basicConfig(
+        format="{asctime} | {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{",
+        level="INFO",
+    )
+    _main()
