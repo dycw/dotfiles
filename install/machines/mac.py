27587fc6ca9697bb7f3d98249529bac8b403c72f
Derek Wan <d.wan@icloud.com>
2025-10-07 18:20:52 +0900

    '2025-10-07 18:20:52 (Tue)  >  RH-Mac  >  derekwan'

--- install/machines/mac.py
+++ install/machines/mac.py
@@ -1,15 +1,21 @@
 from __future__ import annotations
 
 from logging import getLogger
+from typing import TYPE_CHECKING
 
 from install.machines.common import setup_common
+
+if TYPE_CHECKING:
+    from install.types import PathLike
 
 _LOGGER = getLogger(__name__)
 
 
-def setup_mac() -> None:
+def setup_mac(
+    *, git_config: PathLike | None = None, git_ignore: PathLike | None = None
+) -> None:
     _LOGGER.info("Setting up Mac...")
-    setup_common()
+    setup_common(git_config=git_config, git_ignore=git_ignore)
     # keyboard international
 
 
