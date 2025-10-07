c40d5354fe5a3216527a70f7122c12882f15b06b
Derek Wan <d.wan@icloud.com>
2025-10-07 16:56:14 +0900

    '2025-10-07 16:56:14 (Tue)  >  RH-Mac  >  derekwan'

--- install/machines/macbook.py
+++ install/machines/macbook.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+from logging import basicConfig, getLogger
+
+_LOGGER = getLogger(__name__)
 
 
 def main() -> None:
-    pass
+    _LOGGER.info("Setting up Macbook...")
 
 
 if __name__ == "__main__":
+    basicConfig(
+        format="{asctime} | {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{",
+        level="INFO",
+    )
     main()
