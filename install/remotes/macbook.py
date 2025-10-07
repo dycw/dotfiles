83e232cf7ec56a2de085dfefad6a9460d45d772a
Derek Wan <d.wan@icloud.com>
2025-10-07 17:21:53 +0900

    '2025-10-07 17:21:52 (Tue)  >  RH-Mac  >  derekwan'

--- install/remotes/macbook.py
+++ install/remotes/macbook.py
@@ -27,34 +27,24 @@ class _Settings:
         return _Settings(**vars(parser.parse_args()))
 
 
-def main(*, remote: bool = False) -> None:
-    if remote:
-        _LOGGER.info("Setting up Macbook remotely...")
-        with TemporaryDirectory() as _temp_dir:
-            temp_dir = Path(_temp_dir)
-            _LOGGER.info("Cloning repo...")
-            url = "https://github.com/dycw/dotfiles.git"
-            dotfiles = temp_dir / "dotfiles"
-            _ = check_call(
-                f"git clone {url} {dotfiles}",
-                stdout=DEVNULL,
-                stderr=DEVNULL,
-                shell=True,
-            )
-            _ = check_call(  # TEMPORARY!
-                "git checkout re-organize",
-                stdout=DEVNULL,
-                stderr=DEVNULL,
-                shell=True,
-                cwd=dotfiles,
-            )
-            _ = check_call(
-                "python3 -m install.machines.macbook", shell=True, cwd=dotfiles
-            )
-    else:
-        from install.machines.macbook import setup_macbook
-
-        setup_macbook()
+def _main() -> None:
+    _LOGGER.info("Setting up Macbook remotely...")
+    with TemporaryDirectory() as _temp_dir:
+        temp_dir = Path(_temp_dir)
+        _LOGGER.info("Cloning repo...")
+        url = "https://github.com/dycw/dotfiles.git"
+        dotfiles = temp_dir / "dotfiles"
+        _ = check_call(
+            f"git clone {url} {dotfiles}", stdout=DEVNULL, stderr=DEVNULL, shell=True
+        )
+        _ = check_call(  # TEMPORARY!
+            "git checkout re-organize",
+            stdout=DEVNULL,
+            stderr=DEVNULL,
+            shell=True,
+            cwd=dotfiles,
+        )
+        _ = check_call("python3 -m install.machines.macbook", shell=True, cwd=dotfiles)
 
 
 if __name__ == "__main__":
@@ -64,5 +54,4 @@ if __name__ == "__main__":
         style="{",
         level="INFO",
     )
-    settings = _Settings.parse()
-    main(remote=settings.remote)
+    _main()
