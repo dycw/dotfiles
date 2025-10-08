7a92aa79b512cc674252a9ac4b08062dfb4d74e1
Derek Wan <d.wan@icloud.com>
2025-10-08 09:52:30 +0900

    '2025-10-08 09:52:30 (Wed)  >  RH-Mac  >  derekwan'

--- install/groups/common.py
+++ install/groups/common.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from logging import getLogger
 from typing import TYPE_CHECKING
 
-from install.lib import add_to_known_hosts, setup_pdb, setup_psql, setup_sshd
+from install.lib import add_to_known_hosts, setup_pdb, setup_psql
 
 if TYPE_CHECKING:
     from install.types import PathLike
