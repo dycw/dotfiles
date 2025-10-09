4f4a289d8b50ed8f6f5a14cffd4ecc6c4e41b5d8
Derek Wan <d.wan@icloud.com>
2025-10-09 21:09:12 +0900

    Fix sourcing of `work-gitlab/infra` (#619)

--- fish/work.fish
+++ fish/work.fish
@@ -9,7 +9,7 @@ set -l infra
 if test -d $HOME/work/infra
     set infra $HOME/work/infra
 else if test -d $HOME/work-gitlab/infra
-    set infra $HOME/work/infra-gitlab
+    set infra $HOME/work-gitlab/infra
 end
 if set -q infra; and test -f $infra/shell/fish.fish
     source $infra/shell/fish.fish
