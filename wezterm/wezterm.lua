c7d0292e27f1f6d2d18cfef2b9cacc673bacc444
Derek Wan <d.wan@icloud.com>
2025-10-18 20:18:54 +0900

    '2025-10-18 20:18:54 (Sat)  > DW-Mac  > derekwan'

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,6 +15,19 @@ config.keys = {
         action = wezterm.action.DisableDefaultAssignment,
     },
 }
+config.skip_close_confirmation_for_processes_named = {
+    "bash",
+    "sh",
+    "zsh",
+    "fish",
+    "tmux",
+    "nu",
+    "cmd.exe",
+    "pwsh.exe",
+    "powershell.exe",
+    "proxmox",
+    "wezterm",
+}
 config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
     left = "8",
