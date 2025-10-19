1006d14346d320b091ad5c3afcec8aaf0ba6c05f
Derek Wan <d.wan@icloud.com>
2025-10-19 20:02:31 +0900

    '2025-10-19 20:02:31 (Sun)  > DW-Swift  > derek'

--- fish/config.fish
+++ fish/config.fish
@@ -493,6 +493,9 @@ if type -q tailscale; and type -q tailscaled
     function ts-no-exit-node
         sudo tailscale set --exit-node=
     end
+    function wts
+        watch --color --differences --interval=0.5 -- tailscale status
+    end
 end
 
 # tmux
--- install/groups/common.py
+++ install/groups/common.py
@@ -3,7 +3,14 @@ from __future__ import annotations
 from logging import getLogger
 from typing import TYPE_CHECKING
 
-from install.lib import add_to_known_hosts, setup_pdb, setup_psql, setup_ssh, setup_sshd
+from install.lib import (
+    add_to_known_hosts,
+    setup_pdb,
+    setup_psql,
+    setup_resolv_conf,
+    setup_ssh,
+    setup_sshd,
+)
 
 if TYPE_CHECKING:
     from install.types import PathLike
@@ -16,11 +23,14 @@ def setup_common(
     pdbrc: PathLike | None = None,
     permit_root_login: bool = False,
     psqlrc: PathLike | None = None,
+    resolv_conf: PathLike | None = None,
+    resolv_conf_immutable: bool = False,
 ) -> None:
     _LOGGER.info("Setting up common...")
     add_to_known_hosts()
     setup_pdb(pdbrc=pdbrc)
     setup_psql(psqlrc=psqlrc)
+    setup_resolv_conf(resolv_conf=resolv_conf, immutable=resolv_conf_immutable)
     setup_ssh()
     setup_sshd(permit_root_login=permit_root_login)
 
