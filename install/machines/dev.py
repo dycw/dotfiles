fe015a02b46f512d906e1645a8b7d17fbce57c85
Derek Wan <d.wan@icloud.com>
2025-10-29 20:52:30 +0900

    2025-10-29 20:52:30 (Wed)  > DW-Mac  > derekwan

---
+++ install/machines/dev.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from dataclasses import dataclass
+from logging import basicConfig, getLogger
+from pathlib import Path
+
+from install.constants import (
+    FISH_CONFIG,
+    FISH_ENV,
+    FISH_GIT,
+    FISH_WORK,
+    FZF_FISH,
+    GIT_CONFIG,
+    GIT_IGNORE,
+    NVIM,
+    PDBRC,
+    PSQLRC,
+    RIPGREPRC,
+    STARSHIP_TOML,
+    TMUX_CONF_LOCAL,
+    TMUX_CONF_OH_MY_TMUX,
+    WEZTERM_LUA,
+)
+from install.groups.linux import setup_linux
+from install.lib import install_spotify
+
+_LOGGER = getLogger(__name__)
+_PATH_MACHINES = Path(__file__).parent
+_PATH_INSTALL = _PATH_MACHINES.parent
+_PATH_CONFIGS = _PATH_INSTALL / "configs"
+_PATH_LINUX = _PATH_CONFIGS / "linux"
+_RESOLV_CONF = _PATH_LINUX / "resolv.conf"
+
+
+@dataclass(order=True, unsafe_hash=True, kw_only=True, slots=True)
+class _Settings:
+    verbose: bool
+
+    @classmethod
+    def parse(cls) -> _Settings:
+        parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+        _ = parser.add_argument(
+            "-ve", "--verbose", action="store_true", help="Verbose mode."
+        )
+        return _Settings(**vars(parser.parse_args()))
+
+
+def _setup_swift() -> None:
+    _LOGGER.info("Setting up Swift...")
+    setup_linux(
+        fish_config=FISH_CONFIG,
+        fish_env=FISH_ENV,
+        fish_git=FISH_GIT,
+        fish_work=FISH_WORK,
+        fzf_fish=FZF_FISH,
+        git_config=GIT_CONFIG,
+        git_ignore=GIT_IGNORE,
+        nvim_dir=NVIM,
+        pdbrc=PDBRC,
+        psqlrc=PSQLRC,
+        resolv_conf=_RESOLV_CONF,
+        resolv_conf_immutable=True,
+        ripgreprc=RIPGREPRC,
+        starship_toml=STARSHIP_TOML,
+        tmux_conf_oh_my_tmux=TMUX_CONF_OH_MY_TMUX,
+        tmux_conf_local=TMUX_CONF_LOCAL,
+        wezterm_lua=WEZTERM_LUA,
+    )
+    install_spotify()
+
+
+if __name__ == "__main__":
+    settings = _Settings.parse()
+    basicConfig(
+        format="{asctime} | {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{",
+        level="DEBUG" if settings.verbose else "INFO",
+    )
+    _setup_swift()
