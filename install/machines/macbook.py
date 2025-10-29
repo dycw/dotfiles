f155408b3d0184c0907586c78af09bd7311ba647
Derek Wan <d.wan@icloud.com>
2025-10-29 21:31:42 +0900

    2025-10-29 21:31:42 (Wed)  > DW-Mac  > derekwan

--- install/machines/macbook.py
+++ install/machines/macbook.py
@@ -5,25 +5,13 @@ from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from dataclasses import dataclass
 from logging import basicConfig, getLogger
 
+from install.constants import (
+    REPO_STARSHIP_TOML,
+    REPO_TMUX_CONF_LOCAL,
+    REPO_TMUX_CONF_OH_MY_TMUX,
+)
 from install.groups.mac import setup_mac
 from install.lib import install_dropbox, install_zoom
-from install.more_constants import (
-    FISH_CONFIG,
-    FISH_ENV,
-    FISH_GIT,
-    FISH_WORK,
-    FZF_FISH,
-    GIT_CONFIG,
-    GIT_IGNORE,
-    NVIM,
-    PDBRC,
-    PSQLRC,
-    RIPGREPRC,
-    STARSHIP_TOML,
-    TMUX_CONF_LOCAL,
-    TMUX_CONF_OH_MY_TMUX,
-    WEZTERM_LUA,
-)
 
 _LOGGER = getLogger(__name__)
 
@@ -44,21 +32,9 @@ class _Settings:
 def _setup_macbook() -> None:
     _LOGGER.info("Setting up MacBook...")
     setup_mac(
-        fish_config=FISH_CONFIG,
-        fish_env=FISH_ENV,
-        fish_git=FISH_GIT,
-        fish_work=FISH_WORK,
-        fzf_fish=FZF_FISH,
-        git_config=GIT_CONFIG,
-        git_ignore=GIT_IGNORE,
-        nvim_dir=NVIM,
-        pdbrc=PDBRC,
-        psqlrc=PSQLRC,
-        ripgreprc=RIPGREPRC,
-        starship_toml=STARSHIP_TOML,
-        tmux_conf_oh_my_tmux=TMUX_CONF_OH_MY_TMUX,
-        tmux_conf_local=TMUX_CONF_LOCAL,
-        wezterm_lua=WEZTERM_LUA,
+        starship_toml=REPO_STARSHIP_TOML,
+        tmux_conf_oh_my_tmux=REPO_TMUX_CONF_OH_MY_TMUX,
+        tmux_conf_local=REPO_TMUX_CONF_LOCAL,
     )
     install_dropbox()
     install_zoom()
