516716fc25ebef99c24211bd9d0420473328f717
Derek Wan <d.wan@icloud.com>
2025-11-13 17:23:59 +0900

    2025-11-13 17:23:59 (Thu)  > DW-Mac  > derekwan

--- install/constants.py
+++ install/constants.py
@@ -2,36 +2,42 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from .installer_constants import (
-    AUTHORIZED_KEYS,
-    BASHRC,
-    CONFIG_BOTTOM_TOML,
-    CONFIG_DIRENV,
-    CONFIG_FD_IGNORE,
-    CONFIG_FISH,
-    CONFIG_FISH_CONF_D,
-    CONFIG_FISH_FUNCTIONS,
-    CONFIG_GIT,
-    CONFIG_GLAB_CONFIG_YML,
-    CONFIG_NVIM,
-    CONFIG_SOPS_AGE,
-    CONFIG_STARSHIP_TOML,
-    CONFIG_TMUX,
-    CONFIG_TMUX_CONF_LOCAL,
-    CONFIG_TMUX_CONF_OH_MY_TMUX,
-    CONFIG_WEZTERM_LUA,
-    ETC,
-    HOME,
-    KNOWN_HOSTS,
-    LOCAL_BIN,
-    PDBRC,
-    PSQLRC,
-    RESOLV_CONF,
-    SSH,
-    SSH_CONFIG,
-    SSH_CONFIG_D,
-    XDG_CONFIG_HOME,
-)
+ETC = Path("/etc")
+HOME = Path("~").expanduser()
+
+
+ETC_SSH = ETC / "ssh"
+LOGROTATE_D = ETC / "logrotate.d"
+RESOLV_CONF = ETC / "resolv.conf"
+BASHRC = HOME / ".bashrc"
+LOCAL_BIN = HOME / ".local/bin"
+PDBRC = HOME / ".pdbrc"
+PSQLRC = HOME / ".psqlrc"
+SSH = HOME / ".ssh"
+XDG_CONFIG_HOME = HOME / ".config"
+
+
+SSHD_CONFIG = ETC_SSH / "sshd_config"
+AUTHORIZED_KEYS = SSH / "authorized_keys"
+SSH_CONFIG = SSH / "config"
+SSH_CONFIG_D = SSH / "config.d"
+KNOWN_HOSTS = SSH / "known_hosts"
+CONFIG_BOTTOM_TOML = XDG_CONFIG_HOME / "bottom/bottom.toml"
+CONFIG_DIRENV = XDG_CONFIG_HOME / "direnv"
+CONFIG_FD_IGNORE = XDG_CONFIG_HOME / "fd/ignore"
+CONFIG_FISH = XDG_CONFIG_HOME / "fish"
+CONFIG_FISH_CONF_D = CONFIG_FISH / "conf.d"
+CONFIG_FISH_FUNCTIONS = CONFIG_FISH / "functions"
+CONFIG_GIT = XDG_CONFIG_HOME / "git"
+CONFIG_GLAB_CONFIG_YML = XDG_CONFIG_HOME / "glab-cli/config.yml"
+CONFIG_NVIM = XDG_CONFIG_HOME / "nvim"
+CONFIG_SOPS_AGE = XDG_CONFIG_HOME / "sops/age/keys.txt"
+CONFIG_STARSHIP_TOML = XDG_CONFIG_HOME / "starship.toml"
+CONFIG_TMUX = XDG_CONFIG_HOME / "tmux"
+CONFIG_TMUX_CONF_OH_MY_TMUX = CONFIG_TMUX / "tmux.conf"
+CONFIG_TMUX_CONF_LOCAL = CONFIG_TMUX / "tmux.conf.local"
+CONFIG_WEZTERM_LUA = XDG_CONFIG_HOME / "wezterm/wezterm.lua"
+
 
 PATH_INSTALL = Path(__file__).parent
 REPO_ROOT = PATH_INSTALL.parent
@@ -90,10 +96,12 @@ __all__ = [
     "CONFIG_TMUX_CONF_OH_MY_TMUX",
     "CONFIG_WEZTERM_LUA",
     "ETC",
+    "ETC_SSH",
     "HOME",
     "KNOWN_HOSTS",
     "LINUX_RESOLV_CONF",
     "LOCAL_BIN",
+    "LOGROTATE_D",
     "PATH_CONFIGS",
     "PATH_INSTALL",
     "PDBRC",
@@ -127,6 +135,7 @@ __all__ = [
     "REPO_WEZTERM_LUA",
     "RESOLV_CONF",
     "SSH",
+    "SSHD_CONFIG",
     "SSH_CONFIG",
     "SSH_CONFIG_D",
     "XDG_CONFIG_HOME",
--- install/types.py
+++ install/types.py
@@ -1,1 +1,8 @@
-installer/src/installer/types.py
+from __future__ import annotations
+
+from pathlib import Path
+
+type PathLike = Path | str
+
+
+__all__ = ["PathLike"]
