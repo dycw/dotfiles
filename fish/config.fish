d9e1eb878e799fdbb9767958961b1c14c666e8ce
Derek Wan <d.wan@icloud.com>
2025-12-02 09:43:19 +0900

    Add `curl-ip` alias and `<Leader>lr` (#728)

--- fish/config.fish
+++ fish/config.fish
@@ -145,11 +145,16 @@ function cp
 end
 
 # curl
-function curl-sh
-    if test (count $argv) -lt 1
-        echo "'curl-sh' expected [1..) arguments URL; got $(count $argv)" >&2; and return 1
+if type -q curl
+    function curl-sh
+        if test (count $argv) -lt 1
+            echo "'curl-sh' expected [1..) arguments URL; got $(count $argv)" >&2; and return 1
+        end
+        curl -fsSL $argv[1] | sh -s -- $argv[2..]
     end
-    curl -fsSL $argv[1] | sh -s -- $argv[2..]
+    function curl-ip
+        curl api.ipify.org
+    end
 end
 
 # direnv
