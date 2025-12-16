3bbe7fb6f5ae5c2633ff9b2fe24a5144f35be08a
Derek Wan <d.wan@icloud.com>
2025-12-16 20:35:27 +0900

    Move to `gitea` even if `gitlab.main`, and source `$HOME/work/qrt-dotfiles/fish/config.fish` (#756)

--- fish/work.fish
+++ fish/work.fish
@@ -5,7 +5,9 @@ if ! status is-interactive
 end
 
 # work
-if test -f $HOME/infra/shell/infra.fish
+if test -f $HOME/work/qrt-dotfiles/fish/config.fish
+    export INFRA_FISH_CONFIG=$HOME/work/qrt-dotfiles/fish/config.fish
+else if test -f $HOME/infra/shell/infra.fish
     export INFRA_FISH_CONFIG=$HOME/infra/shell/infra.fish
 else if test -f $HOME/work/infra/shell/infra.fish
     export INFRA_FISH_CONFIG=$HOME/work/infra/shell/infra.fish
