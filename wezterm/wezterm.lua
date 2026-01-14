16ee57ec3cf194b49d7c696a274b25fe88bf5d1c
Derek Wan <d.wan@icloud.com>
2026-01-15 05:54:52 +0900

    2026-01-15 05:54:52 (Thu)  > DW-Mac  > derekwan

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 -- always
 config.audible_bell = "Disabled"
-config.font_size = 13.0
+config.font_size = 12.0
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 200
 config.initial_rows = 100
@@ -42,7 +42,7 @@ if wezterm.hostname() == "DW-Swift" then
 end
 
 if wezterm.hostname() == "RH-Mac" then
-    config.font_size = 12.5 -- smaller
+    config.font_size = 12.0
 end
 
 if wezterm.hostname() == "RH-MacBook" then
