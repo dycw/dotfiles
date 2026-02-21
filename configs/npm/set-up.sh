26d087d6154f9c8ebec62b3171f70d0a7196bee7
Derek Wan <d.wan@icloud.com>
2026-02-21 23:05:39 +0900

    Add `~/.npm-global` to `$PATH` (#849)
    Co-authored-by: github-actions-bot <noreply@github.com>

---
+++ configs/npm/set-up.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -eu
+
+###############################################################################
+
+link() {
+	src="$(dirname -- "$(realpath -- "$0")")/$1"
+	dest="${XDG_CONFIG_HOME:-${HOME}/.config}/$2"
+	mkdir -p "$(dirname -- "${dest}")"
+	ln -sfn "${src}" "${dest}"
+}
+
+###############################################################################
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Setting up 'npm'..."
+
+link shell.fish fish/conf.d/npm.fish
