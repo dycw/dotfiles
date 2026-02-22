2f4756e103063c320d3c859f2b754a9b488d4075
Derek Wan <d.wan@icloud.com>
2026-02-22 10:26:25 +0900

    Standardize `wezterm` font size (#858)
    Co-authored-by: github-actions-bot <noreply@github.com>

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -38,21 +38,12 @@ config.window_padding = {
 }
 
 -- conditional
-if wezterm.hostname() == "DW-Swift" then
-    config.font_size = 10.5 -- smaller
-end
-
-if wezterm.hostname() == "RH-Mac" then
-    config.font_size = 12.0
-end
-
 if wezterm.hostname() == "RH-MacBook" then
     config.allow_win32_input_mode = false
     config.animation_fps = 1
     config.cursor_blink_ease_in = "Constant"
     config.cursor_blink_ease_out = "Constant"
     config.enable_scroll_bar = false
-    config.font_size = 11.0 -- smaller
     config.freetype_load_target = "Light"
     config.freetype_render_target = "HorizontalLcd"
     config.hide_tab_bar_if_only_one_tab = true
